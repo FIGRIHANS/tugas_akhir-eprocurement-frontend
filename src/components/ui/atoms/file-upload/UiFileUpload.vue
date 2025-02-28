7038b0d5e1555b350d7573dc3a8c0026d788fe56
haidar-datasea <m.haidar@datasea.co.id>
2025-02-28 18:04:55 +0700

    update truncate and max size to upload

--- src/components/ui/atoms/file-upload/UiFileUpload.vue
+++ src/components/ui/atoms/file-upload/UiFileUpload.vue
@@ -1,23 +1,24 @@
 <template>
-  <div class="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+  <div :class="{ 'flex items-center flex-wrap lg:flex-nowrap gap-2.5': label }">
     <label class="form-label w-2/5">
       {{ label }}
     </label>
 
-    <label for="upload-file" class="w-full">
+    <label :for="name" class="w-full">
       <div class="input-group w-full">
         <span class="btn btn-input bg-primary-clarity border rounded-r-none border-primary"
-          ><i class="ki-filled ki-file-up !text-primary"> </i
+          ><i class="ki-filled ki-cloud-add !text-primary"> </i
         ></span>
         <span
           class="input select-none !text-primary bg-primary-light border-primary hover:border-primary"
+          :title="selectedFile"
         >
-          {{ selectedFile }}
+          {{ truncateSelectedFile }}
         </span>
       </div>
     </label>
     <input
-      id="upload-file"
+      :id="name"
       class="hidden"
       :disabled="disabled"
       :accept="acceptedFiles"
@@ -29,10 +30,11 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import type { IFileUploadProps } from './types/file-upload'
 
 const props = withDefaults(defineProps<IFileUploadProps>(), {
+  name: 'upload-file',
   placeholder: '',
   disabled: false,
 })
@@ -40,9 +42,20 @@ const emits = defineEmits(['addedFile'])
 
 const selectedFile = ref(props.placeholder)
 
+const truncateSelectedFile = computed(() => {
+  if (props.textLength && selectedFile.value !== props.placeholder) {
+    return selectedFile.value.substring(0, props.textLength) + '...'
+  }
+
+  return selectedFile.value
+})
+
 const uploadFile = (event: Event) => {
   const filesUploaded = event.target as HTMLInputElement
-  if (filesUploaded.files && filesUploaded.files?.length > 0) {
+  const isBelowMaxSize =
+    filesUploaded.files && props.maxSize ? filesUploaded.files[0].size <= props.maxSize : true
+
+  if (filesUploaded.files && filesUploaded.files?.length > 0 && isBelowMaxSize) {
     selectedFile.value = filesUploaded.files[0].name
     emits('addedFile', filesUploaded.files[0])
   }
--- src/components/ui/atoms/file-upload/types/file-upload.ts
+++ src/components/ui/atoms/file-upload/types/file-upload.ts
@@ -1,6 +1,9 @@
 export interface IFileUploadProps {
+  name?: string
   label?: string
   placeholder?: string
   disabled?: boolean
   acceptedFiles?: string
+  textLength?: number
+  maxSize?: number
 }
