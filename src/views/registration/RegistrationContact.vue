cf7486c6bb9c82f76cb3e3eb4fbaea0ff73a0f08
haidar-datasea <m.haidar@datasea.co.id>
2025-03-07 22:25:40 +0700

    restyle, add error, and add logic for table in Contact

--- src/views/registration/RegistrationContact.vue
+++ src/views/registration/RegistrationContact.vue
@@ -6,34 +6,68 @@
         label="Username"
         placeholder="Masukkan Username"
         row
-      />
-      <UiInput v-model="contact.account.email" label="Email" placeholder="Masukkan Email" row />
-      <UiInput
-        v-model="contact.account.password"
-        label="Password"
-        placeholder="Masukkan Password"
-        row
+        required
+        :error="contact.account.usernameError"
       />
       <UiInput
-        v-model="contact.account.confirmPassword"
-        label="Ulangi Password"
-        placeholder="Ulangi Password"
+        v-model="contact.account.email"
+        label="Email"
+        placeholder="Masukkan Email"
         row
+        required
+        :error="contact.account.emailError"
       />
-
+      <div class="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+        <label class="form-label w-2/5 flex items-center gap-1">
+          Password
+          <span class="text-danger"> * </span>
+        </label>
+        <div class="input" :class="{ 'border-danger': contact.account.passwordError }">
+          <input
+            v-model="contact.account.password"
+            placeholder="Masukkan Password"
+            :type="showPassword ? 'text' : 'password'"
+          />
+          <div class="btn btn-icon" @click="togglePassword('password')">
+            <i class="ki-outline" :class="showPassword ? 'ki-eye-slash' : 'ki-eye'"></i>
+          </div>
+        </div>
+      </div>
+      <div class="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+        <label class="form-label w-2/5 flex items-center gap-1">
+          Ulangi Password
+          <span class="text-danger"> * </span>
+        </label>
+        <div class="input" :class="{ 'border-danger': contact.account.confirmPasswordError }">
+          <input
+            v-model="contact.account.confirmPassword"
+            placeholder="Ulangi Password"
+            :type="showConfirmPassword ? 'text' : 'password'"
+            @input="checkConfirmPassword"
+          />
+          <div class="btn btn-icon" @click="togglePassword('confirmPassword')">
+            <i class="ki-outline" :class="showConfirmPassword ? 'ki-eye-slash' : 'ki-eye'"></i>
+          </div>
+        </div>
+      </div>
       <UiInput
         v-model="contact.account.website"
         label="Website"
         placeholder="Masukkan website"
         row
       />
-      <UiInput
+      <UiInputTel
         v-model="contact.account.noTel"
         label="No Telephone"
         placeholder="Masukkan no telephone"
         row
+        required
+        :error="contact.account.noTelError"
+        :options="[
+          { value: '+62', text: '+62' },
+          { value: '+80', text: '+80' },
+        ]"
       />
-      <UiInput v-model="contact.account.noFax" label="No Fax" placeholder="Masukkan no fax" row />
     </UiFormGroup>
 
     <UiFormGroup hide-border>
@@ -43,18 +77,28 @@
           label="Nama Lengkap"
           placeholder="Masukkan nama lengkap"
           row
+          required
+          :error="contact.contactPerson.fullNameError"
         />
-        <UiInput
+        <UiInputTel
           v-model="contact.contactPerson.noTel"
           label="No Telephone"
           placeholder="Masukkan no telephone"
           row
+          required
+          :error="contact.contactPerson.noTelError"
+          :options="[
+            { value: '+62', text: '+62' },
+            { value: '+80', text: '+80' },
+          ]"
         />
         <UiInput
           v-model="contact.contactPerson.email"
           label="Email"
           placeholder="Masukkan email"
           row
+          required
+          :error="contact.contactPerson.emailError"
         />
         <UiSelect
           v-model="contact.contactPerson.position"
@@ -64,6 +108,8 @@
           value-key="positionTypeId"
           text-key="positionName"
           row
+          required
+          :error="contact.contactPerson.positionError"
         />
       </UiFormGroup>
 
@@ -114,7 +160,7 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, onMounted } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 
 import { useRegistrationVendorStore } from '@/stores/views/registration'
 import { useVendorMasterDataStore } from '@/stores/master-data/vendor-master-data'
@@ -124,27 +170,63 @@ import UiInput from '@/components/ui/atoms/input/UiInput.vue'
 import UiSelect from '@/components/ui/atoms/select/UiSelect.vue'
 import UiButton from '@/components/ui/atoms/button/UiButton.vue'
 import UiIcon from '@/components/ui/atoms/icon/UiIcon.vue'
+import UiInputTel from '@/components/ui/atoms/input-telephone/UiInputTel.vue'
 
 const registrationVendorStore = useRegistrationVendorStore()
 const vendorMasterDataStore = useVendorMasterDataStore()
 
+const showPassword = ref<boolean>(false)
+const showConfirmPassword = ref<boolean>(false)
+
 const contact = computed(() => registrationVendorStore.contact)
 const positionList = computed(() => vendorMasterDataStore.posistionList)
 
+const togglePassword = (type: 'password' | 'confirmPassword') => {
+  if (type === 'password') {
+    showPassword.value = !showPassword.value
+  } else {
+    showConfirmPassword.value = !showConfirmPassword.value
+  }
+}
+
+const checkConfirmPassword = () => {
+  const { password, confirmPassword } = contact.value.account
+  const isPasswordSame = confirmPassword === password
+
+  if (isPasswordSame) {
+    registrationVendorStore.contact.account.confirmPasswordError = false
+  } else {
+    registrationVendorStore.contact.account.confirmPasswordError = true
+  }
+}
+
 const addContactPerson = () => {
-  const { list, ...clearObject } = contact.value.contactPerson
+  const { list, fullNameError, noTelError, emailError, positionError, ...clearObject } =
+    contact.value.contactPerson
 
   const isObjectHasEmpty = Object.values(clearObject).some((value) => !value)
 
-  if (!isObjectHasEmpty) {
+  if (isObjectHasEmpty) {
+    registrationVendorStore.contact.contactPerson = {
+      ...registrationVendorStore.contact.contactPerson,
+      fullNameError: clearObject.fullName === '',
+      noTelError: clearObject.noTel === '',
+      emailError: clearObject.email === '',
+      positionError: clearObject.position === 0,
+    }
+  } else {
     registrationVendorStore.contact.contactPerson.list.push(clearObject)
 
     registrationVendorStore.contact.contactPerson = {
       ...registrationVendorStore.contact.contactPerson,
       fullName: '',
+      fullNameError: false,
       noTel: '',
+      noTelError: false,
       email: '',
+      emailError: false,
       position: 0,
+      positionError: false,
     }
   }
 }
