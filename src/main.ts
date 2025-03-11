d22cef51241e94e175fb1649e31e07b2aa938750
syhbandi <saboonlbk@gmail.com>
2025-03-11 09:53:38 +0800

    add vendor dashboard

--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,14 @@ import middleware from './middleware'
 
 import VueDatePicker from '@vuepic/vue-datepicker'
 import '@vuepic/vue-datepicker/dist/main.css'
+import VueApexCharts from 'vue3-apexcharts'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(middleware)
-
+// app.use(middleware)
+app.use(VueApexCharts);
 app.component('VueDatePicker', VueDatePicker)
 
 app.mount('#app')
--- src/views/vendor/VendorMaster.vue
+++ src/views/vendor/VendorMaster.vue
@@ -10,7 +10,7 @@
   <!-- Vendor AI System -->
   <div v-if="currentTab === 'Vendor AI System'">AI</div>
   <!-- Dashboard -->
-  <div v-if="currentTab === 'Dashboard'">Dashboard</div>
+  <VendorDashboard v-if="currentTab === 'Dashboard'" />
   <!-- Vendor list -->
   <div v-if="currentTab === 'Vendor List'">Vendor List</div>
 </template>
@@ -21,6 +21,7 @@ import UiTab from '@/components/ui/atoms/tab/UiTab.vue'
 import { onMounted, ref } from 'vue'
 
 import VendorInformation from './components/VendorInformation.vue'
+import VendorDashboard from './components/VendorDashboard.vue'
 const tabs = [
   { label: 'Information', value: 'Information' },
   { label: 'Vendor AI System', value: 'Vendor AI System' },
