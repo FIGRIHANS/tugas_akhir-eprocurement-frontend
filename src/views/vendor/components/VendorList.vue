20affbe3d94e32efcd0d7b80d73b89d6a9dd604d
syhbandi <saboonlbk@gmail.com>
2025-03-19 09:52:44 +0800

    add card header

--- src/views/vendor/components/VendorList.vue
+++ src/views/vendor/components/VendorList.vue
@@ -1,7 +1,35 @@
+<script setup lang="ts">
+import UiButton from '@/components/ui/atoms/button/UiButton.vue'
+import UiIcon from '@/components/ui/atoms/icon/UiIcon.vue'
+import UiInputSearch from '@/components/ui/atoms/inputSearch/UiInputSearch.vue'
+</script>
+
 <template>
-  <button
-    class="flex items-center justify-center h-11 px-5 bg-white border-2 border-gray-700 rounded"
-  >
-    Retro button
-  </button>
+  <div class="card">
+    <div class="card-body">
+      <div class="flex items-center justify-between">
+        <UiInputSearch model-value="" placeholder="Cari vendor" />
+        <div class="flex items-center space-x-3">
+          <div
+            class="dropdown"
+            data-dropdown="true"
+            data-dropdown-trigger="click"
+            data-dropdown-placement="bottom-end"
+          >
+            <UiButton class="dropdown-toggle">
+              <UiIcon variant="outline" name="filter" />
+              <span>Filter</span>
+            </UiButton>
+            <div class="dropdown-content w-full max-w-56 p-4">
+              <div>tes</div>
+            </div>
+          </div>
+          <UiButton :outline="true">
+            <UiIcon variant="filled" name="printer" />
+            Export Data Vendor
+          </UiButton>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
