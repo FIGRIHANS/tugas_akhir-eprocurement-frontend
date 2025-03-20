8da1c6bfd804d19031d4b30185ad19336810f39a
syhbandi <saboonlbk@gmail.com>
2025-03-20 10:48:40 +0800

    update: add v-model

--- src/components/ui/atoms/input/UiInput.vue
+++ src/components/ui/atoms/input/UiInput.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="relative">
-    <label v-if="label" class="text-[11px] px-[3px] text-gray-500 bg-white absolute -top-[6px] left-[7px] leading-[12px]">
+    <label
+      v-if="label"
+      class="text-[11px] px-[3px] text-gray-500 bg-white absolute -top-[6px] left-[7px] leading-[12px]"
+    >
       {{ label }}
     </label>
-    <input class="input" :placeholder="placeholder" :readonly="readonly" :disabled="disabled" type="text" value=""/>
+    <input
+      class="input"
+      :placeholder="placeholder"
+      :readonly="readonly"
+      :disabled="disabled"
+      type="text"
+      v-model="model"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
 import type { IInputProps } from './types/input'
+const model = defineModel()
 
 withDefaults(defineProps<IInputProps>(), {
   placeholder: '',
