650ecec3805288c8cf212062330d43c2d2e30437
syhbandi <saboonlbk@gmail.com>
2025-03-21 10:39:42 +0800

    refactor code

--- src/views/vendor/vendorMaster/VendorList.vue
+++ src/views/vendor/vendorMaster/VendorList.vue
@@ -1,30 +1,9 @@
 <script setup lang="ts">
-import DatePicker from '@/components/datePicker/DatePicker.vue'
-import UiButton from '@/components/ui/atoms/button/UiButton.vue'
-import UiIcon from '@/components/ui/atoms/icon/UiIcon.vue'
 import UiInputSearch from '@/components/ui/atoms/inputSearch/UiInputSearch.vue'
-import UiSelect from '@/components/ui/atoms/select/UiSelect.vue'
-import UiInput from '@/components/ui/atoms/input/UiInput.vue'
-import { reactive } from 'vue'
 import LPagination from '@/components/pagination/LPagination.vue'
-import { useRoute, useRouter } from 'vue-router'
-
-const router = useRouter()
-const route = useRoute()
-// const vendorData = reactive([])
-
-const filters = reactive({
-  izinUsahaExp: '',
-})
-
-const handleFilter = () => {
-  router.push({ query: { ...filters } })
-}
-
-const handleReset = () => {
-  filters.izinUsahaExp = ''
-  router.replace({ path: route.path, query: {} })
-}
+import FilterButton from './components/FilterButton.vue'
+import VendorMenu from './components/VendorMenu.vue'
+import StatusToggle from './components/StatusToggle.vue'
 </script>
 
 <template>
@@ -32,64 +11,7 @@ const handleReset = () => {
     <div class="card-header">
       <!-- header -->
       <UiInputSearch model-value="" placeholder="Cari vendor" />
-      <form @submit.prevent="handleFilter">
-        <div class="flex items-center space-x-3">
-          <div
-            class="dropdown"
-            data-dropdown="true"
-            data-dropdown-trigger="click"
-            data-dropdown-placement="bottom-end"
-          >
-            <UiButton class="dropdown-toggle">
-              <UiIcon variant="outline" name="filter" />
-              <span>Filter</span>
-            </UiButton>
-            <div class="dropdown-content w-full max-w-60 p-4 space-y-5">
-              <h1 class="text-lg font-semibold text-gray-700 mb-5">Filter</h1>
-              <UiSelect label="Status" placeholder="Pilih">
-                <option>Select Status</option>
-              </UiSelect>
-              <UiSelect label="Kategori" placeholder="Pilih">
-                <option>Select Status</option>
-              </UiSelect>
-              <UiSelect label="Izin Usaha" placeholder="Pilih">
-                <option>Select Status</option>
-              </UiSelect>
-              <div class="relative">
-                <label
-                  class="text-[11px] px-[3px] text-gray-500 bg-white absolute -top-[6px] left-[7px] leading-[12px] z-5"
-                >
-                  Tanggal Pendaftaran Awal
-                </label>
-                <DatePicker model-value="" />
-              </div>
-              <div class="relative">
-                <label
-                  class="text-[11px] px-[3px] text-gray-500 bg-white absolute -top-[6px] left-[7px] leading-[12px] z-5"
-                >
-                  Tanggal Pendaftaran Akhir
-                </label>
-                <DatePicker model-value="" />
-              </div>
-              <UiInput label="Izin Usaha Expired" v-model="filters.izinUsahaExp" />
-              <div class="flex items-center space-x-3">
-                <UiButton :outline="true" class="flex-1" type="reset" @click="handleReset">
-                  <UiIcon variant="filled" name="arrows-circle" />
-                  Reset
-                </UiButton>
-                <UiButton class="flex-1">
-                  <UiIcon variant="filled" name="check-circle" />
-                  Terapkan
-                </UiButton>
-              </div>
-            </div>
-          </div>
-          <UiButton :outline="true">
-            <UiIcon variant="filled" name="printer" />
-            Export Data Vendor
-          </UiButton>
-        </div>
-      </form>
+      <FilterButton />
       <!-- end of header -->
     </div>
     <div class="card-table scrollable-x-auto">
@@ -110,50 +32,8 @@ const handleReset = () => {
         <tbody>
           <tr>
             <td class="flex items-center gap-5">
-              <div class="dropdown" data-dropdown="true" data-dropdown-trigger="click">
-                <UiButton :outline="true" :icon="true" variant="secondary" class="dropdown-toggle">
-                  <UiIcon variant="outline" name="dots-vertical" />
-                </UiButton>
-                <div class="dropdown-content w-full max-w-60 space-y-5">
-                  <ul class="menu menu-default flex flex-col gap-2">
-                    <li class="menu-item">
-                      <a class="menu-link" href="#">
-                        <span class="menu-icon">
-                          <UiIcon variant="duotone" name="eye" />
-                        </span>
-                        <span class="menu-title"> Detail Vendor </span>
-                      </a>
-                    </li>
-                    <li class="menu-item">
-                      <a class="menu-link" href="#">
-                        <span class="menu-icon">
-                          <UiIcon variant="duotone" name="data" />
-                        </span>
-                        <span class="menu-title"> Detail Verifikasi </span>
-                      </a>
-                    </li>
-                    <div class="border-b border-b-gray-200"></div>
-                    <li class="menu-item">
-                      <a class="menu-link" href="#">
-                        <span class="menu-icon">
-                          <UiIcon variant="duotone" name="cross-circle" class="!text-red-600" />
-                        </span>
-                        <span class="menu-title !text-red-600"> Blacklist </span>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-
-              <label class="switch">
-                <input
-                  name="check"
-                  type="checkbox"
-                  value="1"
-                  class="switch-on:checked:!bg-success"
-                />
-                <span class="switch-label"> Not Active </span>
-              </label>
+              <VendorMenu :id="1" />
+              <StatusToggle :id="1" />
             </td>
             <td>-</td>
             <td>-</td>
