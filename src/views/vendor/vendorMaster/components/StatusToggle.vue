238c8fe031c1b82e85b8b82b63da80c1f9b7f9bc
syhbandi <saboonlbk@gmail.com>
2025-03-21 10:39:23 +0800

    add status toggle

---
+++ src/views/vendor/vendorMaster/components/StatusToggle.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{ id: string | number }>()
+</script>
+<template>
+  <label class="switch">
+    <input name="check" type="checkbox" value="1" class="switch-on:checked:!bg-success" />
+    <span class="switch-label"> Not Active </span>
+  </label>
+</template>
