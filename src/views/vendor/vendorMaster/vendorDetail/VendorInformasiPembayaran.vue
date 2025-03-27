5516b32da9d277bf915f25951ba2c58d69f65fca
syhbandi <saboonlbk@gmail.com>
2025-03-27 14:31:56 +0800

    add payment page

--- src/views/vendor/vendorMaster/vendorDetail/VendorInformasiPembayaran.vue
+++ src/views/vendor/vendorMaster/vendorDetail/VendorInformasiPembayaran.vue
@@ -1,1 +1,18 @@
-<template>hai</template>
+<script setup lang="ts">
+import UiButton from '@/components/ui/atoms/button/UiButton.vue'
+import UiIcon from '@/components/ui/atoms/icon/UiIcon.vue'
+import VendorPaymentInformationCard from '@/components/vendor/vendorPaymentInformationCard/VendorPaymentInformationCard.vue'
+</script>
+
+<template>
+  <div class="space-y-5">
+    <VendorPaymentInformationCard />
+    <div class="flex justify-end gap-3">
+      <UiButton :outline="true">
+        <UiIcon name="black-left" variant="duotone" />
+        Back</UiButton
+      >
+      <UiButton><UiIcon name="check-squared" variant="duotone" />Verify</UiButton>
+    </div>
+  </div>
+</template>
