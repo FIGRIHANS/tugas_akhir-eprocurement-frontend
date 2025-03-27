103bfeaadb9558cce90bb6428cb26226b08be22e
syhbandi <saboonlbk@gmail.com>
2025-03-27 14:43:25 +0800

    add payment info page

--- src/views/vendor/vendorMaster/vendorDetail/VendorInformasiPembayaran.vue
+++ src/views/vendor/vendorMaster/vendorDetail/VendorInformasiPembayaran.vue
@@ -2,16 +2,23 @@
 import UiButton from '@/components/ui/atoms/button/UiButton.vue'
 import UiIcon from '@/components/ui/atoms/icon/UiIcon.vue'
 import VendorPaymentInformationCard from '@/components/vendor/vendorPaymentInformationCard/VendorPaymentInformationCard.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const handleBack = () => {
+  router.go(-1)
+}
 </script>
 
 <template>
   <div class="space-y-5">
     <VendorPaymentInformationCard />
     <div class="flex justify-end gap-3">
-      <UiButton :outline="true">
+      <UiButton :outline="true" @click="handleBack">
         <UiIcon name="black-left" variant="duotone" />
-        Back</UiButton
-      >
+        <span> Back </span>
+      </UiButton>
       <UiButton><UiIcon name="check-squared" variant="duotone" />Verify</UiButton>
     </div>
   </div>
