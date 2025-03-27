f1f6b1475b72432589f1b639ad3c1973d4f943a7
syhbandi <saboonlbk@gmail.com>
2025-03-27 14:30:55 +0800

    add izin usaha page

--- src/views/vendor/vendorMaster/vendorDetail/VendorIzinUsaha.vue
+++ src/views/vendor/vendorMaster/vendorDetail/VendorIzinUsaha.vue
@@ -1,1 +1,18 @@
-<template>hai</template>
+<script setup lang="ts">
+import UiButton from '@/components/ui/atoms/button/UiButton.vue'
+import UiIcon from '@/components/ui/atoms/icon/UiIcon.vue'
+import VendorIzinUsahaCard from '@/components/vendor/vendorIzinUsahaCard/VendorIzinUsahaCard.vue'
+</script>
+
+<template>
+  <div class="space-y-5">
+    <VendorIzinUsahaCard />
+    <div class="flex justify-end gap-3">
+      <UiButton :outline="true">
+        <UiIcon name="black-left" variant="duotone" />
+        <span> Back </span>
+      </UiButton>
+      <UiButton><UiIcon name="check-squared" variant="duotone" /><span>Verify</span></UiButton>
+    </div>
+  </div>
+</template>
