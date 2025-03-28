dd282bf74b9a798e92f4765deea656de7e69965a
syhbandi <saboonlbk@gmail.com>
2025-03-28 14:33:06 +0800

    add data perlengkapan

--- src/views/vendor/vendorMaster/vendorDetail/VendorPerlengkapan.vue
+++ src/views/vendor/vendorMaster/vendorDetail/VendorPerlengkapan.vue
@@ -1,1 +1,95 @@
-<template>hai</template>
+<script setup lang="ts">
+import LPagination from '@/components/pagination/LPagination.vue'
+import UiButton from '@/components/ui/atoms/button/UiButton.vue'
+import UiIcon from '@/components/ui/atoms/icon/UiIcon.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
+<template>
+  <div class="space-y-5">
+    <div class="card">
+      <div class="card-header">
+        <div class="card-title">Peralatan</div>
+      </div>
+      <div class="card-table scrollable-x-auto">
+        <table class="table align-middle table-border">
+          <thead>
+            <tr>
+              <th class="text-nowrap">
+                <span class="sort">
+                  <span class="sort-label"> Nama Alat </span>
+                  <span class="sort-icon"> </span>
+                </span>
+              </th>
+              <th class="text-nowrap">
+                <span class="sort">
+                  <span class="sort-label"> Merk / Tipe </span>
+                  <span class="sort-icon"> </span>
+                </span>
+              </th>
+              <th class="text-nowrap">
+                <span class="sort">
+                  <span class="sort-label"> Tahun Pembuatan </span>
+                  <span class="sort-icon"> </span>
+                </span>
+              </th>
+              <th class="text-nowrap">
+                <span class="sort">
+                  <span class="sort-label"> No Seri / No Polisi </span>
+                  <span class="sort-icon"> </span>
+                </span>
+              </th>
+              <th class="text-nowrap">
+                <span class="sort">
+                  <span class="sort-label"> Kapasitas (Tonase) </span>
+                  <span class="sort-icon"> </span>
+                </span>
+              </th>
+              <th class="text-nowrap">
+                <span class="sort">
+                  <span class="sort-label"> Kondisi </span>
+                  <span class="sort-icon"> </span>
+                </span>
+              </th>
+              <th class="text-nowrap">
+                <span class="sort">
+                  <span class="sort-label"> Status Kepemilikan </span>
+                  <span class="sort-icon"> </span>
+                </span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>-</td>
+              <td>-</td>
+              <td>-</td>
+              <td>-</td>
+              <td>-</td>
+              <td>-</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="card-footer">
+        <div class="text-gray-600 text-sm gap-3 flex items-center">
+          <span>Show</span>
+          <select class="select w-20">
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="30">30</option>
+          </select>
+          <span class="">per page</span>
+        </div>
+        <LPagination :current-page="1" :page-size="10" :total-items="30" />
+      </div>
+    </div>
+    <div class="flex justify-end">
+      <UiButton :outline="true" @click="router.go(-1)">
+        <UiIcon name="black-left" variant="duotone" />
+        <span>Back</span>
+      </UiButton>
+    </div>
+  </div>
+</template>
