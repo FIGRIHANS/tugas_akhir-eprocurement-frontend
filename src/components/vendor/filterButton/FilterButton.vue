1b21e27845f0b5d20d37860d3667a338a2d8bca9
syhbandi <saboonlbk@gmail.com>
2025-04-09 16:09:50 +0800

    refactor: make filters list more dinamic

--- src/components/vendor/filterButton/FilterButton.vue
+++ src/components/vendor/filterButton/FilterButton.vue
@@ -1,22 +1,61 @@
 <script setup lang="ts">
 import UiIcon from '@/components/ui/atoms/icon/UiIcon.vue'
-import { useRoute, useRouter } from 'vue-router'
-import type { IFilterButtonProps } from './types/filterButton'
+import { useRoute, useRouter, type LocationQueryValue } from 'vue-router'
+import type { IFilterButton } from './types/filterButton'
+import { ref, watch } from 'vue'
 
 const route = useRoute()
 const router = useRouter()
-const props = defineProps<IFilterButtonProps>()
 
-const handleRemoveFilter = () => {
+const filters = ref<IFilterButton[]>([])
+
+watch(
+  () => route.query,
+  (query) => {
+    filters.value = Object.entries(query)
+      .filter(([key]) => key !== 'page')
+      .filter(([key]) => key !== 'search')
+      .map(([key, value]) => ({ key, value }))
+  },
+  { immediate: true, deep: true },
+)
+
+const handleRemoveFilter = (key: string, value?: LocationQueryValue) => {
   const query = { ...route.query }
-  delete query[props.filter.key]
+
+  if (Array.isArray(query[key])) {
+    const newKey = query[key].filter((current) => current !== value)
+    query[key] = newKey
+  } else {
+    delete query[key]
+  }
+
   router.push({ query })
 }
 </script>
 <template>
-  <div class="btn btn-outline btn-secondary">
-    <span class="text-gray-500">{{ filter.key }}</span>
-    <span class="font-semibold">{{ filter.value }}</span>
-    <UiIcon name="cross" @click="handleRemoveFilter" />
+  <div v-if="filters.length > 0" class="flex gap-3 flex-wrap mb-3 items-center">
+    <h3 class="font-semibold text-lg">Filter</h3>
+    <!-- iterate over filters -->
+    <template v-for="filter in filters" :key="filter.key">
+      <!-- if filters value is array then iterate -->
+      <template v-if="Array.isArray(filter.value)">
+        <div
+          class="btn btn-outline btn-secondary"
+          v-for="value in filter.value"
+          :key="value?.toString()"
+        >
+          <span class="text-gray-500">{{ filter.key }}</span>
+          <span class="font-semibold">{{ value }}</span>
+          <UiIcon name="cross" @click="handleRemoveFilter(filter.key, value)" />
+        </div>
+      </template>
+      <!-- else -->
+      <div class="btn btn-outline btn-secondary" v-else>
+        <span class="text-gray-500">{{ filter.key }}</span>
+        <span class="font-semibold">{{ filter.value }}</span>
+        <UiIcon name="cross" @click="handleRemoveFilter(filter.key)" />
+      </div>
+    </template>
   </div>
 </template>
--- src/components/vendor/filterButton/types/filterButton.ts
+++ src/components/vendor/filterButton/types/filterButton.ts
@@ -1,9 +1,5 @@
 import type { LocationQueryValue } from "vue-router"
 
-export interface IFilterButtonProps{
-  filter: IFilterButton
-}
-
 export interface IFilterButton {
   key: string
   value: LocationQueryValue | LocationQueryValue[]
