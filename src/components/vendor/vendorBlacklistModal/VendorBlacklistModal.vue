99fb2f59bd2e3bcda9d9af90bdb8a0d7e02897ef
syhbandi <saboonlbk@gmail.com>
2025-04-14 15:43:23 +0800

    update: add start and end date period

--- src/components/vendor/vendorBlacklistModal/VendorBlacklistModal.vue
+++ src/components/vendor/vendorBlacklistModal/VendorBlacklistModal.vue
@@ -1,31 +1,67 @@
 <script setup lang="ts">
 import UiModal from '@/components/modal/UiModal.vue'
 import type { IVendorBlacklistModalProps } from './types/vendorBlacklistModal'
-import UiSelect from '@/components/ui/atoms/select/UiSelect.vue'
 import UiTextArea from '@/components/ui/atoms/text-area/UiTextArea.vue'
 import { ref } from 'vue'
 import UiButton from '@/components/ui/atoms/button/UiButton.vue'
 import UiIcon from '@/components/ui/atoms/icon/UiIcon.vue'
+import DatePicker from '@/components/datePicker/DatePicker.vue'
+
 defineProps<IVendorBlacklistModalProps>()
 defineEmits(['close'])
 
 const period = ref<string>('')
 const file = ref<File>()
 const reason = ref<string>('')
+const tglMulai = ref('')
+const tglSelesai = ref('')
 
-const handleSubmit = () => {}
+const handleSubmit = () => {
+  console.log(file.value)
+}
 </script>
 <template>
   <UiModal :open="open" @close="$emit('close')" title="Blacklist Vendor" size="sm">
     <form action="" class="space-y-4" @submit.prevent="handleSubmit">
-      <UiSelect label="Blacklist Period" v-model="period"></UiSelect>
+      <div class="relative">
+        <label for="period" class="absolute top-0 left-0 -mt-2 ml-2 bg-white px-1 text-gray-500"
+          >Period</label
+        >
+        <select id="period" v-model="period" class="select">
+          <option disabled value="">Pilih periode</option>
+          <option value="permanent">Permanent</option>
+          <option value="temporary">Temporary</option>
+        </select>
+      </div>
+      <div v-if="period === `temporary`" class="flex gap-3 max-w-full">
+        <DatePicker
+          v-model="tglMulai"
+          :error="false"
+          class="flex-1"
+          placeholder="Start Date"
+          label="Start Date"
+        />
+        <DatePicker
+          v-model="tglSelesai"
+          :error="false"
+          class="flex-1"
+          placeholder="End Date"
+          label="End Date"
+        />
+      </div>
       <div class="flex rounded-md overflow-hidden border border-primary">
         <label
           for="file"
           class="flex items-center px-3 text-primary bg-opacity-20 bg-blue-400 flex-1"
         >
-          <div>Upload supporting files</div>
-          <input type="file" name="file" id="file" hidden />
+          <div>{{ file ? file.name : 'Upload supporting files' }}</div>
+          <input
+            type="file"
+            name="file"
+            id="file"
+            hidden
+            @input="file = ($event.target as HTMLInputElement)?.files?.[0]"
+          />
         </label>
         <button
           type="button"
@@ -34,6 +70,7 @@ const handleSubmit = () => {}
           Upload file
         </button>
       </div>
+
       <UiTextArea label="Reason" :model-value="reason" placeholder="Enter reason" />
       <div class="flex gap-3">
         <UiButton
