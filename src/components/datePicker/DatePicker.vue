a212c2f2717554addece5c392473ba47d65db56a
syhbandi <saboonlbk@gmail.com>
2025-04-14 15:42:56 +0800

    add label and placholder props for better ui

--- src/components/datePicker/DatePicker.vue
+++ src/components/datePicker/DatePicker.vue
@@ -3,7 +3,16 @@
     <VueDatePicker v-model="date" class="w-full">
       <template #dp-input="{ value }">
         <div class="input relative">
-          <input placeholder="Select" :value="value" readonly class="min-w-[0px]" :class="{ 'border-danger': error }"/>
+          <div class="absolute top-0 left-0 -mt-2 ml-2 bg-white px-1 text-gray-500">
+            {{ label ?? 'Date' }}
+          </div>
+          <input
+            :placeholder="placeholder ?? 'Select'"
+            :value="value"
+            readonly
+            class="min-w-[0px]"
+            :class="{ 'border-danger': error }"
+          />
           <button class="btn btn-icon">
             <i class="ki-filled ki-calendar"></i>
           </button>
@@ -20,6 +29,8 @@ import { ref, watch } from 'vue'
 const props = defineProps<{
   modelValue: string
   error?: boolean
+  label?: string
+  placeholder?: string
 }>()
 
 const emits = defineEmits(['update:modelValue'])
@@ -30,14 +41,14 @@ watch(
   () => props.modelValue,
   () => {
     date.value = props.modelValue
-  }
+  },
 )
 
 watch(
   () => date.value,
   () => {
     emits('update:modelValue', date.value)
-  }
+  },
 )
 </script>
 
