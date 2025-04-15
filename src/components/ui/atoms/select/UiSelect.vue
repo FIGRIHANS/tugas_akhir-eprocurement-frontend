13237d1d27b947faac369c1abeb3e3aa899c2a04
syhbandi <saboonlbk@gmail.com>
2025-04-15 15:03:23 +0800

    add slot

--- src/components/ui/atoms/select/UiSelect.vue
+++ src/components/ui/atoms/select/UiSelect.vue
@@ -1,22 +1,17 @@
 <template>
   <div class="relative">
-    <label v-if="label" class="text-[11px] px-[3px] text-gray-500 bg-white absolute -top-[6px] left-[7px] leading-[12px]">
+    <label
+      v-if="label"
+      class="text-[11px] px-[3px] text-gray-500 bg-white absolute -top-[6px] left-[7px] leading-[12px]"
+    >
       {{ label }}
     </label>
 
-    <select class="select" name="select" :readonly="readonly" :disabled="disabled">
-      <option selected disabled hidden>
+    <select class="select" name="select" :readonly="readonly" :disabled="disabled" v-model="model">
+      <option value="">
         {{ placeholder }}
       </option>
-      <option value="1">
-        Option 1
-      </option>
-      <option value="2">
-        Option 2
-      </option>
-      <option value="3">
-        Option 3
-      </option>
+      <slot></slot>
     </select>
   </div>
 </template>
@@ -24,6 +19,7 @@
 <script lang="ts" setup>
 import type { ISelectProps } from './types/select'
 
+const model = defineModel()
 withDefaults(defineProps<ISelectProps>(), {
   placeholder: '',
   readonly: false,
