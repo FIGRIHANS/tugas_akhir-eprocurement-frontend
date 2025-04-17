860103819fa0dcd6b4ce2ce4c473b5611b18919e
syhbandi <saboonlbk@gmail.com>
2025-04-17 10:11:55 +0800

    refactor: add approval history

--- src/components/vendor/verificationModal/VendorVerificationModal.vue
+++ src/components/vendor/verificationModal/VendorVerificationModal.vue
@@ -1,35 +1,69 @@
 <script setup lang="ts">
 import UiModal from '@/components/modal/UiModal.vue'
-import type { IVendorVerificationProps } from './types/vendorVerification'
 
-defineProps<IVendorVerificationProps>()
+defineProps<{ id: string | number }>()
 const open = defineModel()
 defineEmits(['close'])
 </script>
 <template>
-  <UiModal v-model="open" title="Detail Verification Vendor">
-    <h3 class="text-center text-lg">Vendor: PT Agung sejahtera</h3>
-    <div class="modal-table scrollable-auto">
-      <table class="table align-middle">
-        <thead>
-          <tr class="border-b-2 border-b-primary">
-            <th>Nama Verificator</th>
-            <th>Jabatan</th>
-            <th>Tanggal Verifikasi</th>
-            <th>Status</th>
-            <th>Keterangan</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-          </tr>
-        </tbody>
-      </table>
+  <UiModal v-model="open" title="Approval Verifikasi Vendor" size="lg">
+    <div class="space-y-5 mb-5">
+      <h3 class="text-center text-lg">Detail Verifikasi PT Agung sejahtera</h3>
+      <div class="modal-table scrollable-auto">
+        <table class="table align-middle">
+          <thead>
+            <tr class="border-b-2 border-b-primary text-nowrap">
+              <th>No</th>
+              <th>Nama Verificator</th>
+              <th>Jabatan</th>
+              <th>Tanggal Verifikasi</th>
+              <th>Jenis Verifikasi</th>
+              <th>Status</th>
+              <th>Keterangan</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Susi Susanti</td>
+              <td>Manajer Keuangan</td>
+              <td>2021-03-03</td>
+              <td>Vendor Approval</td>
+              <td>On Proccess</td>
+              <td>Oke Sesuai dengan SOP</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="space-y-5">
+      <h3 class="text-center text-lg">Detail Approval PT Agung sejahtera</h3>
+      <div class="modal-table scrollable-auto">
+        <table class="table align-middle">
+          <thead>
+            <tr class="border-b-2 border-b-primary text-nowrap">
+              <th>No</th>
+              <th>Nama Approver</th>
+              <th>Jabatan</th>
+              <th>Tanggal Approval</th>
+              <th>Jenis Approval</th>
+              <th>Status</th>
+              <th>Keterangan</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Susi Susanti</td>
+              <td>Kepala cabang</td>
+              <td>2021-03-03</td>
+              <td>Payment Approval</td>
+              <td>On Proccess</td>
+              <td>Oke Sesuai dengan SOP</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </UiModal>
 </template>
--- src/components/vendor/verificationModal/types/vendorVerification.ts
+++
@@ -1,3 +0,0 @@
-export interface IVendorVerificationProps{
-  id: string | number;
-}
