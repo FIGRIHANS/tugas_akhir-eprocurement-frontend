b6456a3f2a6e8a77b47c9e8514c46457acc7350d
syhbandi <saboonlbk@gmail.com>
2025-04-21 18:11:11 +0800

    setup approval store

---
+++ src/stores/vendor/types/approval.ts
@@ -0,0 +1,19 @@
+export interface IApprovalItem {
+  vendorId: string
+  vendorName: string
+  addressCompanyInfo: string
+  companyCategoryName: string
+  activedUTCDate: string
+  sendApprovalDate: null | string
+  approvalStatus: null | string
+  approvalStatusName: null | string
+  approvalTypeName: null | string
+  businessFieldName: null | string
+}
+
+export interface IApproval {
+  items: IApprovalItem[]
+  total: number
+  page: number
+  pageSize: number
+}
