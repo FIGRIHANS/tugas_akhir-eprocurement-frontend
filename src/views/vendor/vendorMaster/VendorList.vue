db1e111402895afe9b1db38f72ab32196c9eea6c
syhbandi <saboonlbk@gmail.com>
2025-04-21 10:01:23 +0800

    update: integrate vendor-list API

--- src/views/vendor/vendorMaster/VendorList.vue
+++ src/views/vendor/vendorMaster/VendorList.vue
@@ -18,22 +18,22 @@ const router = useRouter()
 const vendor = useVendorStore()
 
 const search = ref('')
-const currentPage = ref(1)
+const currentPage = ref(vendor.vendorList?.page)
 
 const handleSearch = debounce((value) => {
   const query = { ...route.query }
   if (!value) {
-    delete query.search
-    router.push({ query })
+    delete query.SearchQuery
+    router.replace({ query: { ...query, page: 1 } })
     return
   }
 
-  router.push({ query: { ...query, search: value } })
+  router.replace({ query: { ...query, SearchQuery: value, page: 1 } })
 }, 500)
 
 const handlePageChange = (page: number) => {
   const query = { ...route.query, page }
-  router.push({ query })
+  router.replace({ query })
 }
 
 watch(search, handleSearch)
@@ -41,10 +41,10 @@ watch(search, handleSearch)
 watch(
   () => route.query,
   (query) => {
-    search.value = (query.search as string) || ''
+    search.value = (query.SearchQuery as string) || ''
     currentPage.value = Number(query.page) || 1
 
-    vendor.getVendors()
+    vendor.getVendors(query)
   },
   {
     immediate: true,
@@ -57,7 +57,6 @@ watch(
   <div>
     <div class="card">
       <div class="card-header">
-        <!-- header -->
         <UiInputSearch v-model="search" placeholder="Cari vendor" />
         <div class="flex gap-3">
           <FilterDropdown />
@@ -66,8 +65,6 @@ watch(
             Export Data Vendor
           </UiButton>
         </div>
-
-        <!-- end of header -->
       </div>
       <div class="card-body scrollable-x-auto">
         <FilterButton />
@@ -81,11 +78,11 @@ watch(
               <th class="text-nowrap">Tanggal Pendaftaran</th>
               <th class="text-nowrap">Tanggal Permintaan Verifikasi</th>
               <th class="text-nowrap">Tanggal Verifikasi</th>
-              <th class="text-nowrap">Kode E-Procurement Vendor</th>
               <th class="text-nowrap">Kode Vendor</th>
             </tr>
           </thead>
           <tbody>
+            <!-- show loading -->
             <tr v-if="vendor.loading">
               <td colspan="8">
                 <div
@@ -93,23 +90,36 @@ watch(
                 ></div>
               </td>
             </tr>
+
+            <!-- show error -->
+            <tr v-else-if="vendor.error">
+              <td colspan="8">
+                {{ vendor.error }}
+              </td>
+            </tr>
+
+            <!-- show message if there are no data -->
+            <tr v-else-if="!vendor.vendorList?.items.length">
+              <td colspan="8" class="text-center">No data</td>
+            </tr>
+
+            <!-- show data -->
             <tr
               v-else
-              v-for="vendor in vendor.vendorList"
-              :key="vendor.id"
+              v-for="vendor in vendor.vendorList.items"
+              :key="vendor.vendorId"
               class="font-normal text-sm"
             >
               <td class="flex items-center gap-5">
-                <VendorMenu :id="vendor.id" />
-                <StatusToggle :id="vendor.id" :status="true" />
+                <VendorMenu :id="vendor.vendorId" />
+                <StatusToggle :id="vendor.vendorId" :status="true" />
               </td>
               <td class="text-nowrap">{{ vendor.vendorName }}</td>
-              <td>-</td>
+              <td>{{ vendor.approvalStatusName }}</td>
               <td>{{ vendor.companyCategoryName }}</td>
-              <td>{{ vendor.createdUTCDate }}</td>
-              <td>{{ vendor.createdUTCDate }}</td>
-              <td>{{ vendor.createdUTCDate }}</td>
-              <td>{{ vendor.userId }}</td>
+              <td>{{ vendor.activedUTCDate }}</td>
+              <td>{{ vendor.sendApprovalDate }}</td>
+              <td>{{ vendor.sendApprovalDate }}</td>
               <td>{{ vendor.vendorId }}</td>
             </tr>
           </tbody>
@@ -118,11 +128,14 @@ watch(
       <div
         class="card-footer justify-center md:justify-between flex-col md:flex-row gap-3 text-gray-600 text-2sm font-medium"
       >
-        <div>Tampilkan 10 data dari total data 7575</div>
+        <div>
+          Tampilkan {{ vendor.vendorList?.pageSize }} data dari total data
+          {{ vendor.vendorList?.total }}
+        </div>
         <LPagination
-          :total-items="30"
-          :current-page="currentPage"
-          :page-size="10"
+          :total-items="Number(vendor.vendorList?.total)"
+          :current-page="Number(currentPage)"
+          :page-size="Number(vendor.vendorList?.pageSize)"
           @page-change="handlePageChange"
         />
       </div>
