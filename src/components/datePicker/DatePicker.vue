9090d6027b972972187b287eb9ab99f7a9c71d89
syhbandi <saboonlbk@gmail.com>
2025-04-22 10:49:54 +0800

    revert all changes

--- src/components/datePicker/DatePicker.vue
+++ src/components/datePicker/DatePicker.vue
@@ -1,20 +1,13 @@
 <template>
-  <div class="min-w-[0px] w-full">
-    <VueDatePicker v-model="date" class="w-full">
+  <div class="min-w-[0px] w-full flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+    <label class="form-label flex items-center gap-1 w-2/5">
+      {{ label }}
+      <span v-if="required" class="text-danger"> * </span>
+    </label>
+    <VueDatePicker v-model="date" :format="format" :preview-format="format" class="w-full">
       <template #dp-input="{ value }">
-        <div class="input relative">
-          <div
-            :class="['absolute top-0 left-0 -mt-2 ml-2 bg-white px-1 text-gray-500', labelClass]"
-          >
-            {{ label ?? 'Date' }}
-          </div>
-          <input
-            :placeholder="placeholder ?? 'Select'"
-            :value="value"
-            readonly
-            class="min-w-[0px]"
-            :class="{ 'border-danger': error }"
-          />
+        <div class="input relative" :class="{ 'border-danger': error }">
+          <input :placeholder="placeholder" :value="value" readonly class="min-w-[0px]" />
           <button class="btn btn-icon">
             <i class="ki-filled ki-calendar"></i>
           </button>
@@ -32,8 +25,9 @@ const props = defineProps<{
   modelValue: string
   error?: boolean
   label?: string
+  required?: boolean
   placeholder?: string
-  labelClass?: string
+  format?: string
 }>()
 
 const emits = defineEmits(['update:modelValue'])
