5bc5e555280a4677638d412eafee682b35e93cc7
syhbandi <saboonlbk@gmail.com>
2025-05-03 12:28:38 +0800

    update: configure response data

--- src/stores/vendor/upload.ts
+++ src/stores/vendor/upload.ts
@@ -1,16 +1,36 @@
+import type { ApiResponse } from '@/core/type/api'
 import vendorAPI from '@/core/utils/vendorAPI'
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export interface IUploadResponse {
+  name: string
+  url: string
+  urlWithToken: string
+}
 
 export const useVendorUploadStore = defineStore('vendor-upload', () => {
+  const loading = ref(false)
+  const error = ref<string | null>(null)
   const upload = async (formData: FormData) => {
+    loading.value = true
+    error.value = null
     try {
-      const response = await vendorAPI.postForm('/api/file/upload', formData, {
-        headers: { 'Content-Type': 'multipart/form-data' },
-      })
-      return response.data
-    } catch (error) {
-      console.error('Error uploading file:', error)
+      const response: ApiResponse<IUploadResponse> = await vendorAPI.post(
+        '/api/file/upload',
+        formData,
+        {
+          headers: { 'Content-Type': 'multipart/form-data' },
+        },
+      )
+      return response.data.result.content
+    } catch (err) {
+      if (err instanceof Error) {
+        error.value = err.message
+      }
+    } finally {
+      loading.value = false
     }
   }
-  return { upload }
+  return { upload, loading, error }
 })
