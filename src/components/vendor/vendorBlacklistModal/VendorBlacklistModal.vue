b9ea243966625b8a2b1dc48cdea7190b308528d0
syhbandi <saboonlbk@gmail.com>
2025-05-03 12:28:52 +0800

    configure simple validation

--- src/components/vendor/vendorBlacklistModal/VendorBlacklistModal.vue
+++ src/components/vendor/vendorBlacklistModal/VendorBlacklistModal.vue
@@ -5,7 +5,8 @@ import { onMounted, ref } from 'vue'
 import UiButton from '@/components/ui/atoms/button/UiButton.vue'
 import UiIcon from '@/components/ui/atoms/icon/UiIcon.vue'
 import { useBlacklistPeriodStore } from '@/stores/vendor/reference'
-import { useVendorUploadStore } from '@/stores/vendor/upload'
+import { useVendorUploadStore, type IUploadResponse } from '@/stores/vendor/upload'
+import UiLoading from '@/components/UiLoading.vue'
 
 const periodStore = useBlacklistPeriodStore()
 const uploadStore = useVendorUploadStore()
@@ -17,6 +18,8 @@ const file = ref<File>()
 const reason = ref<string>('')
 const tglMulai = ref<Date>()
 const tglSelesai = ref<Date>()
+const uploadedFile = ref<IUploadResponse>()
+const inputError = ref<string[]>([])
 
 const handleUpload = async () => {
   const formData = new FormData()
@@ -25,12 +28,28 @@ const handleUpload = async () => {
   formData.append('FolderName', '')
   formData.append('FileName', file.value?.name as string)
 
-  const upload = await uploadStore.upload(formData)
-  console.log(upload)
+  uploadedFile.value = await uploadStore.upload(formData)
 }
 
 const handleSubmit = () => {
-  console.log(file.value)
+  inputError.value = []
+
+  if (!period.value) {
+    inputError.value.push('period')
+  }
+
+  if (period.value === 'temporary') {
+    if (!tglMulai.value) {
+      inputError.value.push('tglMulai')
+    }
+    if (!tglSelesai.value) {
+      inputError.value.push('tglSelesai')
+    }
+  }
+
+  if (!reason.value) {
+    inputError.value.push('reason')
+  }
 }
 
 onMounted(periodStore.getPeriod)
@@ -42,14 +61,15 @@ onMounted(periodStore.getPeriod)
         <label for="period" class="absolute top-0 left-0 -mt-2 ml-2 bg-white px-1 text-gray-500">
           Period
         </label>
-        <select id="period" v-model="period" class="select">
+        <select id="period" v-model="period" class="select" required>
           <option disabled value="">Select period</option>
           <option v-for="item in periodStore.period" :key="item.code" :value="item.code">
             {{ item.value }}
           </option>
         </select>
+        <span v-if="inputError.includes('period')">Period is Required</span>
       </div>
-      <div v-if="period === `temporary`" class="flex gap-3 max-w-full">
+      <div v-if="period === `0`" class="flex gap-3 max-w-full">
         <VueDatePicker v-model="tglMulai" class="w-full">
           <template #dp-input="{ value }">
             <div class="input relative">
@@ -65,6 +85,7 @@ onMounted(periodStore.getPeriod)
                 <i class="ki-filled ki-calendar"></i>
               </button>
             </div>
+            <span v-if="inputError.includes('tglMulai')">Start Date is Required</span>
           </template>
         </VueDatePicker>
         <VueDatePicker v-model="tglSelesai" class="w-full">
@@ -83,12 +104,13 @@ onMounted(periodStore.getPeriod)
               </button>
             </div>
           </template>
+          <span v-if="inputError.includes('tglSelesai')">End Date is Required</span>
         </VueDatePicker>
       </div>
       <div class="flex rounded-md overflow-hidden border border-primary">
         <label
           for="file"
-          class="flex items-center px-3 text-primary bg-opacity-20 bg-blue-400 flex-1"
+          class="flex items-center px-3 text-primary bg-opacity-20 bg-blue-400 flex-1 cursor-pointer"
         >
           <div>{{ file ? file.name : 'Upload supporting files' }}</div>
           <input
@@ -96,22 +118,34 @@ onMounted(periodStore.getPeriod)
             name="file"
             id="file"
             hidden
+            :disabled="uploadStore.loading"
             @input="file = ($event.target as HTMLInputElement)?.files?.[0]"
           />
         </label>
         <button
           type="button"
-          class="h-10 bg-primary text-white flex items-center justify-center px-3 border border-primary"
+          class="h-10 bg-primary text-white flex items-center justify-center px-3 border border-primary disabled:opacity-50 disabled:cursor-not-allowed"
           @click="handleUpload"
+          :disabled="uploadStore.loading || !file"
         >
-          Upload file
+          <UiLoading size="sm" v-if="uploadStore.loading" />
+          <span v-else>Upload</span>
         </button>
       </div>
+      <span class="text-danger text-[12px]" v-if="uploadStore.error">{{ uploadStore.error }}</span>
+      <span class="text-success text-[12px]" v-if="uploadedFile?.name">Upload file success</span>
       <div class="relative">
         <label for="reason" class="absolute -mt-2 ml-2 px-1 text-gray-500 bg-white">
           Reason <span class="text-danger">*</span>
         </label>
-        <textarea id="reason" v-model="reason" class="textarea textarea-lg" rows="6"></textarea>
+        <textarea
+          id="reason"
+          v-model="reason"
+          class="textarea textarea-lg"
+          rows="6"
+          required
+        ></textarea>
+        <span v-if="inputError.includes('reason')">Reason is Required</span>
       </div>
       <div class="flex gap-3">
         <UiButton class="flex-1 justify-center" :outline="true" type="button" @click="open = !open">
