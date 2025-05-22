4efa8f225c1518c907fc752003168cb83e1c79b8
syhbandi <saboonlbk@gmail.com>
2025-05-22 16:59:56 +0800

    add notify vendor modal

--- src/components/vendor/VendorMenu.vue
+++ src/components/vendor/VendorMenu.vue
@@ -6,11 +6,14 @@ import VendorVerificationModal from './verificationModal/VendorVerificationModal
 import VendorBlacklistModal from './vendorBlacklistModal/VendorBlacklistModal.vue'
 import UiModal from '../modal/UiModal.vue'
 import successImg from '@/assets/success.svg'
+import NotifyVendorModal from './notifyVendorModal/NotifyVendorModal.vue'
 
 defineProps<{ id: string | number; name: string }>()
 const verificationModalOpen = ref<boolean>(false)
 const blacklistModalOpen = ref<boolean>(false)
 const blacklistSuccessOpen = ref<boolean>(false)
+const notifyVendorModalOpen = ref<boolean>(false)
+const notifyVendorSuccessModalOpen = ref<boolean>(false)
 </script>
 <template>
   <div class="dropdown" data-dropdown="true" data-dropdown-trigger="click">
@@ -37,6 +40,14 @@ const blacklistSuccessOpen = ref<boolean>(false)
         </li>
         <div class="border-b border-b-gray-200"></div>
         <li class="menu-item">
+          <button class="menu-link" @click="notifyVendorModalOpen = true">
+            <span class="menu-icon">
+              <UiIcon variant="duotone" name="notification-bing" class="!text-warning" />
+            </span>
+            <span class="menu-title !text-warning"> Notify Vendor </span>
+          </button>
+        </li>
+        <li class="menu-item">
           <button class="menu-link" @click="blacklistModalOpen = true">
             <span class="menu-icon">
               <UiIcon variant="duotone" name="cross-circle" class="!text-red-600" />
@@ -61,11 +72,24 @@ const blacklistSuccessOpen = ref<boolean>(false)
     v-if="blacklistModalOpen"
   />
 
+  <NotifyVendorModal
+    v-model:open="notifyVendorModalOpen"
+    v-model:success="notifyVendorSuccessModalOpen"
+    v-if="notifyVendorModalOpen"
+  />
+
   <UiModal v-if="blacklistSuccessOpen" v-model="blacklistSuccessOpen" size="sm">
     <img :src="successImg" alt="success" class="mx-auto mb-3" />
     <h3 class="font-medium text-lg text-gray-800 text-center">Vendor Successfully Blacklisted</h3>
     <p class="text-gray-600 text-center mb-3">
       The vendor has been added to the blacklist and is no longer active in the system.
+    </p>
+  </UiModal>
+  <UiModal v-if="notifyVendorSuccessModalOpen" v-model="notifyVendorSuccessModalOpen" size="sm">
+    <img :src="successImg" alt="success" class="mx-auto mb-3" />
+    <h3 class="font-medium text-lg text-gray-800 text-center">Vendor Successfully Notified</h3>
+    <p class="text-gray-600 text-center mb-3">
+      Your remark has been delivered via email to the vendor’s contact.
     </p>
   </UiModal>
 </template>
