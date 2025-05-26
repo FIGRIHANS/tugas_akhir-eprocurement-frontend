de461767f78bef670c1ee32c49c6bfd326cec15b
syhbandi <saboonlbk@gmail.com>
2025-05-26 15:46:15 +0800

    update lang

--- src/components/vendor/verificationModal/VendorVerificationModal.vue
+++ src/components/vendor/verificationModal/VendorVerificationModal.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import UiModal from '@/components/modal/UiModal.vue'
 import UiLoading from '@/components/UiLoading.vue'
-import { formatDate } from '@/core/utils/format'
 import { useVerificationDetailStore } from '@/stores/vendor/vendor'
+import moment from 'moment'
 import { onMounted } from 'vue'
 
 const verificationStore = useVerificationDetailStore()
@@ -15,20 +15,19 @@ onMounted(() => {
 })
 </script>
 <template>
-  <UiModal v-model="open" title="Approval Verifikasi Vendor" size="lg">
+  <UiModal v-model="open" title="Verification Detail" size="lg">
     <div class="space-y-5 mb-5">
-      <h3 class="text-center text-lg">Detail Verifikasi {{ name }}</h3>
+      <h3 class="text-center text-lg">Vendor: {{ name }}</h3>
       <div class="modal-table scrollable-auto">
         <table class="table align-middle">
           <thead>
             <tr class="border-b-2 border-b-primary text-nowrap">
               <th>No</th>
-              <th>Nama Verificator</th>
-              <th>Jabatan</th>
-              <th>Tanggal Verifikasi</th>
-              <th>Jenis Verifikasi</th>
+              <th>Verificator Name</th>
+              <th>Position</th>
+              <th>Verification Date</th>
               <th>Status</th>
-              <th>Keterangan</th>
+              <th>Description</th>
             </tr>
           </thead>
           <tbody>
@@ -48,39 +47,21 @@ onMounted(() => {
               <td>{{ index + 1 }}</td>
               <td>{{ item.verificatorName }}</td>
               <td>{{ item.position }}</td>
-              <td>{{ item.createdDate ? formatDate(new Date(item.createdDate)) : '-' }}</td>
-              <td>{{ item.verificationType }}</td>
-              <td>{{ item.status }}</td>
+              <td>
+                {{ moment(item.createdDate).format('LLL') }}
+              </td>
+              <td>
+                <span
+                  class="badge badge-outline"
+                  :class="{
+                    'badge-success': item.status === 'Approved',
+                    'badge-danger': item.status === 'Rejected',
+                  }"
+                >
+                  {{ item.status }}
+                </span>
+              </td>
               <td>{{ item.keterangan }}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-    <div class="space-y-5 hidden">
-      <h3 class="text-center text-lg">Detail Approval PT Agung sejahtera</h3>
-      <div class="modal-table scrollable-auto">
-        <table class="table align-middle">
-          <thead>
-            <tr class="border-b-2 border-b-primary text-nowrap">
-              <th>No</th>
-              <th>Nama Approver</th>
-              <th>Jabatan</th>
-              <th>Tanggal Approval</th>
-              <th>Jenis Approval</th>
-              <th>Status</th>
-              <th>Keterangan</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Susi Susanti</td>
-              <td>Kepala cabang</td>
-              <td>2021-03-03</td>
-              <td>Payment Approval</td>
-              <td>On Proccess</td>
-              <td>Oke Sesuai dengan SOP</td>
             </tr>
           </tbody>
         </table>
