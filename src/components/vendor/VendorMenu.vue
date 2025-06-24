b8a04561cbc07e0d32965d99a9f1609849d843fe
syhbandi <saboonlbk@gmail.com>
2025-06-24 14:34:58 +0800

    remove notify vendor

--- src/components/vendor/VendorMenu.vue
+++ src/components/vendor/VendorMenu.vue
@@ -6,7 +6,6 @@ import VendorVerificationModal from './verificationModal/VendorVerificationModal
 import VendorBlacklistModal from './vendorBlacklistModal/VendorBlacklistModal.vue'
 import UiModal from '../modal/UiModal.vue'
 import successImg from '@/assets/success.svg'
-import NotifyVendorModal from './notifyVendorModal/NotifyVendorModal.vue'
 import { useRouter } from 'vue-router'
 
 defineProps<{ id: string | number; name: string; email: string }>()
@@ -14,8 +13,6 @@ const router = useRouter()
 const verificationModalOpen = ref<boolean>(false)
 const blacklistModalOpen = ref<boolean>(false)
 const blacklistSuccessOpen = ref<boolean>(false)
-const notifyVendorModalOpen = ref<boolean>(false)
-const notifyVendorSuccessModalOpen = ref<boolean>(false)
 
 const handleSuccess = () => {
   router.replace({ name: 'vendor-list' })
@@ -46,14 +43,6 @@ const handleSuccess = () => {
         </li>
         <div class="border-b border-b-gray-200"></div>
         <li class="menu-item">
-          <button class="menu-link" @click="notifyVendorModalOpen = true">
-            <span class="menu-icon">
-              <UiIcon variant="duotone" name="notification-bing" class="!text-warning" />
-            </span>
-            <span class="menu-title !text-warning"> Notify Vendor </span>
-          </button>
-        </li>
-        <li class="menu-item">
           <button class="menu-link" @click="blacklistModalOpen = true">
             <span class="menu-icon">
               <UiIcon variant="duotone" name="cross-circle" class="!text-red-600" />
@@ -71,19 +60,12 @@ const handleSuccess = () => {
     v-model="verificationModalOpen"
     v-if="verificationModalOpen"
   />
+
   <VendorBlacklistModal
     :id="id"
     v-model:open="blacklistModalOpen"
     v-model:success="blacklistSuccessOpen"
     v-if="blacklistModalOpen"
-  />
-
-  <NotifyVendorModal
-    v-model:open="notifyVendorModalOpen"
-    v-model:success="notifyVendorSuccessModalOpen"
-    v-if="notifyVendorModalOpen"
-    :vendor-name="name"
-    :vendor-email="email"
   />
 
   <UiModal
@@ -96,18 +78,6 @@ const handleSuccess = () => {
     <h3 class="font-medium text-lg text-gray-800 text-center">Vendor Successfully Blacklisted</h3>
     <p class="text-gray-600 text-center mb-3">
       The vendor has been added to the blacklist and is no longer active in the system.
-    </p>
-  </UiModal>
-  <UiModal
-    v-if="notifyVendorSuccessModalOpen"
-    v-model="notifyVendorSuccessModalOpen"
-    size="sm"
-    @update:model-value="handleSuccess"
-  >
-    <img :src="successImg" alt="success" class="mx-auto mb-3" />
-    <h3 class="font-medium text-lg text-gray-800 text-center">Vendor Successfully Notified</h3>
-    <p class="text-gray-600 text-center mb-3">
-      Your remark has been delivered via email to the vendor’s contact.
     </p>
   </UiModal>
 </template>
