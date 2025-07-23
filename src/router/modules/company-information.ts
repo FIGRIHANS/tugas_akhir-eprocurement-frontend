fa90790d3624febe12df270aa910783f2263bdbf
Muhammad Kuswari <kuswarimuhammad10@gmail.com>
2025-07-23 16:43:44 +0800

    complete slicing ui

--- src/router/modules/company-information.ts
+++ src/router/modules/company-information.ts
@@ -5,11 +5,6 @@ export default [
     component: () => import("@/views/companyInformation/payment-information/PaymentInfoListView.vue")
   },
   {
-    path: "/bussines-license-data",
-    name: "bussiness-license-data-list",
-    component: () => import("@/views/companyInformation/bussiness-license-data/BussinesLicenseDataListView.vue")
-  },
-  {
     path: "/company-deed-data",
     name: "company-deed-data-list",
     component: () => import("@/views/companyInformation/company-deed-data/CompanyDeedDataView.vue")
---
+++ src/views/vendor/companyInformation/BussinesLicenseDataListView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <PKPTableView />
+    <OtherDocTableView />
+  </div>
+</template>
+
+<script setup lang="ts">
+import OtherDocTableView from '@/components/vendor/businessLicenseData/OtherDocTableView.vue'
+import PKPTableView from '@/components/vendor/businessLicenseData/PKPTableView.vue'
+</script>
