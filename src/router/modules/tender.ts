8b60a9a1629837586db10b6fa6c3b83ad60b28a3
Calvin Halim (PT Kamoro) <cavim1998@gmail.com>
2025-07-29 09:02:47 +0700

    temp sidemenu & router

---
+++ src/router/modules/tender.ts
@@ -0,0 +1,17 @@
+export default [
+  {
+    path: '/tender/central',
+    name: 'tenderCentral',
+    // component: () => import('@/views/invoice/InvoiceAddWrapper.vue')
+  },
+  {
+    path: '/tender/report',
+    name: 'tenderReport',
+    // component: () => import('@/views/invoiceVerification/InvoiceVerification.vue')
+  },
+  {
+    path: '/tender/create',
+    name: 'tenderCreate',
+    // component: () => import('@/views/invoiceApproval/InvoiceApproval.vue')
+  }
+]
