9188cc8935e3c764ea6f7ff8d72c2b7b453349a1
Firdaus Mochamad <medioid13@gmail.com>
2025-08-09 16:54:29 +0700

    Vendor Analysis

--- src/views/tenderCreate/TenderCreate/VendorList.vue
+++ src/views/tenderCreate/TenderCreate/VendorList.vue
@@ -3,7 +3,7 @@
     <div class="flex align-items-center justify-between mb-[24px]">
       <div class="input w-[250px]">
         <i class="ki-filled ki-magnifier"></i>
-        <input v-model="search" placeholder="Cari data" type="text"/>
+        <input v-model="search" placeholder="Cari data" type="text" />
       </div>
       <div>
         <button class="btn btn-primary" @click="showAddVendor">
@@ -13,12 +13,8 @@
       </div>
     </div>
     <div v-if="form.vendorList.length === 0" class="text-sm h-[120px] text-center text-gray-800">
-      <p>
-        You must add the vendor first.
-      </p>
-      <p>
-        The vendor will be displayed here.
-      </p>
+      <p>You must add the vendor first.</p>
+      <p>The vendor will be displayed here.</p>
     </div>
     <div v-else class="vendor-list__table">
       <table class="table text-gray-700 font-medium text-sm">
@@ -30,7 +26,7 @@
               class="vendor-list__field-base !border-b-blue-500"
               :class="{
                 'vendor-list__field-base--action': index === 0,
-                'vendor-list__field-base--name': item === 'Vendor Name'
+                'vendor-list__field-base--name': item === 'Vendor Name',
               }"
             >
               {{ item }}
@@ -40,7 +36,10 @@
         <tbody>
           <tr v-for="(data, index) in form.vendorList" :key="data.id">
             <td>
-              <button class="btn btn-icon btn-outline btn-danger w-[32px] h-[32px]" @click="deleteItem(index)">
+              <button
+                class="btn btn-icon btn-outline btn-danger w-[32px] h-[32px]"
+                @click="deleteItem(index)"
+              >
                 <i class="ki-duotone ki-trash !text-lg"></i>
               </button>
             </td>
@@ -82,6 +81,9 @@
               </span>
             </td>
             <td>{{ data.totalPo }}</td>
+            <td>{{ data.yearlySpanding }}</td>
+            <td>{{ data.currency }}</td>
+            <td>{{ data.existingContract }}</td>
           </tr>
         </tbody>
       </table>
@@ -91,6 +93,58 @@
     </div>
     <AddVendorModal @setItem="setItemVendor" />
   </div>
+  <section class="mt-[16px]">
+    <UiButton @click="showAnalysis = true">
+      Vendor Smart Analysis <i class="ki-duotone ki-artificial-intelligence"></i
+    ></UiButton>
+  </section>
+  <section v-if="showAnalysis !== false" name="smart-analysis scrollable-x-auto" class="mt-5">
+    <table class="table-border text-gray-700 font-medium text-sm">
+      <thead class="table-border">
+        <tr>
+          <th rowspan="4" class="header-category table-border">Nama</th>
+          <th colspan="6" class="header-category table-border bg-blue-200">Kategori Analisis</th>
+        </tr>
+        <tr>
+          <th colspan="2" class="header-category table-border">Profil Vendor</th>
+          <th colspan="2" class="header-category table-border">Kinerja Umum</th>
+          <th colspan="2" class="header-category table-border">Rekomendasi AI</th>
+        </tr>
+        <tr>
+          <th colspan="6" class="header-category table-border bg-blue-200">Metrik Terstruktur</th>
+        </tr>
+        <tr>
+          <th class="header-category table-border">Length of Services</th>
+          <th class="header-category table-border">Total Spending</th>
+          <th class="header-category table-border">KPI Utama (3 tahun)</th>
+          <th class="header-category table-border">Kepatuhan SLA</th>
+          <th class="header-category table-border">Analisis High-Level</th>
+          <th class="header-category table-border">Analisis Negosiasi Kontrak</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="data in analysisData" :key="data.vendor_id">
+          <td class="text-center">{{ data.vendor_name }}</td>
+          <td class="text-center">{{ data.lengh_of_sevice }}</td>
+          <td class="text-center">{{ data.total_spending }}</td>
+          <td class="text-center">
+            {{ data.main_kpi }}<br />
+            <span class="badge badge-pill badge-outline badge-danger">{{ data.kpi_quality }}</span>
+          </td>
+          <td>
+            Kualitas: {{ data.sla_quality }} <br />
+            Pengiriman: {{ data.sla_delivery }}
+          </td>
+          <td>
+            <strong>{{ data.risk }}</strong> {{ data.high_level }}
+          </td>
+          <td>
+            {{ data.contract_negotiation }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script lang="ts" setup>
@@ -98,11 +152,13 @@ import { ref, reactive, inject, defineAsyncComponent } from 'vue'
 import { KTModal } from '@/metronic/core'
 import type { FormTypes } from '../types/tenderCreate'
 import type { TableItemTypes } from '../types/vendorList'
+import UiButton from '@/components/ui/atoms/button/UiButton.vue'
 
 const AddVendorModal = defineAsyncComponent(() => import('./VendorList/AddVendor.vue'))
 
 const form = inject<FormTypes>('form')
 const search = ref<string>('')
+const showAnalysis = ref<boolean>(false)
 
 const columns = reactive<string[]>([
   'Action',
@@ -115,7 +171,46 @@ const columns = reactive<string[]>([
   'Lead Time Supply',
   'After Sales Warranty',
   'Order Absorption',
-  'Total PO'
+  'Total PO',
+  'Yearly Spending',
+  'Currency',
+  'Existing Contract',
+])
+
+const analysisData = ref([
+  {
+    vendor_id: '1',
+    vendor_name: 'PT Walldorf Grosshandel Tbk',
+    total_spending: '$100.000',
+    lengh_of_sevice: '5 Tahun',
+    main_kpi: 'Rata-rata 80%',
+    kpi_quality: 'menurun',
+    sla_quality: 'Buruk',
+    sla_delivery: 'Baik',
+    high_level:
+      'Walaupun pengeluaran signifikan dan hubungan jangka panjang, kinerja menurun konsisten. Ada risiko finansial dan operasional jika tren ini berlanjut.',
+    risk: 'Berisiko Tinggi.',
+    contract_negotiation:
+      'Harga: Negosiasikan untuk membekukan harga atau menuntut penurunan 3-5% SLA: Perkuat penalti untuk setiap insiden kualitas Term Condition: Tambahkan opsi pembatalan tanpa penalti jika kualitas tidak terpenuhi.',
+  },
+  // {
+  //   vendor_id: '2',
+  //   vendor_name: null,
+  //   lengh_of_sevice: null,
+  //   main_kpi: null,
+  //   sla: null,
+  //   high_level: null,
+  //   contreact_negotiation: null,
+  // },
+  // {
+  //   vendor_id: '3',
+  //   vendor_name: null,
+  //   lengh_of_sevice: null,
+  //   main_kpi: null,
+  //   sla: null,
+  //   high_level: null,
+  //   contreact_negotiation: null,
+  // },
 ])
 
 const colorRank = (rank: string) => {
@@ -123,7 +218,7 @@ const colorRank = (rank: string) => {
     '1': 'badge-success',
     '2': 'badge-primary',
     '3': 'badge-warning',
-    '4': 'badge-danger'
+    '4': 'badge-danger',
   } as { [key: string]: string }
   return lib[rank]
 }
@@ -147,4 +242,8 @@ const setItemVendor = (items: TableItemTypes[]) => {
 
 <style lang="scss" scoped>
 @use '../styles/vendorList.scss';
+.header-category {
+  text-align: center;
+  font-weight: bold;
+}
 </style>
