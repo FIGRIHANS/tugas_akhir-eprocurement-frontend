85ed77767668088ede5204135a99e49c91320b3d
haidar-datasea <m.haidar@datasea.co.id>
2025-08-14 09:52:45 +0700

    fetching other doc in vendor profile

--- src/components/ui/attachment/AttachmentView.vue
+++ src/components/ui/attachment/AttachmentView.vue
@@ -3,6 +3,7 @@
     <iconPDF />
     <div v-if="fileData">
       <p class="text-sm font-medium mb-[4px]">{{ fileName() }}</p>
+      <p class="text-xs text-gray-700">Uploaded: {{ uploadDate }}</p>
     </div>
   </div>
 </template>
@@ -13,6 +14,7 @@ import type { attachmentFileTypes } from './types/attachment-view'
 
 const props = defineProps<{
   fileData: attachmentFileTypes | null
+  uploadDate?: string
 }>()
 
 const fileName = () => {
---
+++ src/views/companyInformation/details/OtherDocumentData.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="card min-w-full">
+    <div class="card-header">
+      <h3 class="card-title">Other Documents</h3>
+    </div>
+
+    <div class="card-table">
+      <table class="table align-middle">
+        <thead>
+          <tr>
+            <th>Document Name</th>
+            <th>Document Number</th>
+            <th>Available until</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="data in dataResponse" :key="data.id">
+            <td>
+              <AttachmentView
+                :file-data="{ name: data.documentName, path: data.documentUrl }"
+                :upload-date="formatDate(data.issuedUTCDate)"
+              />
+            </td>
+            <td>{{ data.documentNo }}</td>
+            <td>{{ formatDate(data.expiredUTCDate) }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, onMounted, watch } from 'vue'
+
+import useOtherDocStore from '@/stores/vendor/otherDocuments'
+// import { useUploadStore } from '@/stores/general/upload'
+
+import { formatDate } from '@/composables/date-format'
+
+import AttachmentView from '@/components/ui/attachment/AttachmentView.vue'
+
+const props = defineProps<{ vendorId: number | undefined }>()
+
+const otherDocStore = useOtherDocStore()
+// const uploadStore = useUploadStore()
+
+const dataResponse = computed(() => otherDocStore.data)
+
+// const downloadFile = async (path: string) => {
+//   await uploadStore.previewFile(path)
+// }
+
+onMounted(() => {
+  otherDocStore.get(Number(props.vendorId))
+})
+
+watch(
+  () => props.vendorId,
+  () => {
+    otherDocStore.get(Number(props.vendorId))
+  },
+)
+</script>
