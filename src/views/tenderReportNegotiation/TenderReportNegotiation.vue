291fc21fe0adc665488acf3cfb10fa52bcd3d507
Firdaus Mochamad <medioid13@gmail.com>
2025-08-16 13:16:04 +0700

    edit count

--- src/views/tenderReportNegotiation/TenderReportNegotiation.vue
+++ src/views/tenderReportNegotiation/TenderReportNegotiation.vue
@@ -3,8 +3,12 @@
     <Breadcrumb title="Negotiation" :routes="routes" />
     <div class="border rounded-lg p-[24px]">
       <UiTab :items="tabItems" v-model="currentTab" />
-      <AwardingDraft v-if="currentTab === 'awardingDraft'" />
-      <NegotiationSummary v-if="currentTab === 'negotiationSummary'" />
+      <AwardingDraft
+        :data="dummyData"
+        @updateCount="updateQty"
+        v-if="currentTab === 'awardingDraft'"
+      />
+      <NegotiationSummary :data="dummyData" v-if="currentTab === 'negotiationSummary'" />
       <ParticipatingVendor class="mt-[24px]" />
     </div>
   </div>
@@ -42,7 +46,170 @@ const tabItems: ITabItem[] = [
   },
 ]
 
+const dummyData = [
+  {
+    materialCode: 'CH-1062',
+    materialDesc: 'Macro Fertilizer Urea',
+    quantity: 30,
+    uom: 'TON',
+    bottomPriceLBMA: 100861.5,
+    unitPriceLBMA: 3362.0,
+    simulationAmount: 93000.0,
+    variance: '-8%',
+    currency: 'USD',
+    awardDraft: '1 winner',
+    simulationStatus: 'Error',
+    simulationMessage: 'Kumulasi Nego Qty melebihi Tender Qty, silakan disesuaikan kembali',
+    bids: [
+      {
+        rank: 1,
+        vendor: 'PT Walldorf Tbk',
+        negoQty: 30,
+        uom: 'TON',
+        bottomPrice: 100861.5,
+        unitPrice: 3100.0,
+        negotiationAmount: 93000.0,
+        var: '-8%',
+        currency: 'USD',
+        awardStatus: '🏅',
+      },
+      {
+        rank: 2,
+        vendor: 'PT Somper GmbH Tbk',
+        negoQty: 0,
+        uom: 'TON',
+        bottomPrice: null,
+        unitPrice: 3200.0,
+        negotiationAmount: null,
+        var: '0%',
+        currency: 'USD',
+        awardStatus: '',
+      },
+      {
+        rank: 3,
+        vendor: 'PT Kreuzschmid Kgaa Tbk',
+        negoQty: 0,
+        uom: 'TON',
+        bottomPrice: null,
+        unitPrice: 3250.0,
+        negotiationAmount: null,
+        var: '0%',
+        currency: 'USD',
+        awardStatus: '',
+      },
+    ],
+  },
+  {
+    materialCode: 'CH-1064',
+    materialDesc: 'Macro Fertilizer NPK',
+    quantity: 33,
+    uom: 'TON',
+    bottomPriceLBMA: 110947.65,
+    unitPriceLBMA: 3362.05,
+    simulationAmount: 103300.0,
+    variance: '-7%',
+    currency: 'USD',
+    awardDraft: '2 winner',
+    simulationStatus: 'Completed',
+    simulationMessage: 'Kumulasi Nego Qty melebihi Tender Qty, silakan disesuaikan kembali',
+    bids: [
+      {
+        rank: 1,
+        vendor: 'PT Walldorf Tbk',
+        negoQty: 23,
+        uom: 'TON',
+        bottomPrice: 77327.15,
+        unitPrice: 3100.0,
+        negotiationAmount: 71300.0,
+        var: '-8%',
+        currency: 'USD',
+        awardStatus: '🏅',
+      },
+      {
+        rank: 2,
+        vendor: 'PT Somper GmbH Tbk',
+        negoQty: 10,
+        uom: 'TON',
+        bottomPrice: 33620.5,
+        unitPrice: 3200.0,
+        negotiationAmount: 32000.0,
+        var: '-5%',
+        currency: 'USD',
+        awardStatus: '🏅',
+      },
+      {
+        rank: 3,
+        vendor: 'PT Kreuzschmid Kgaa Tbk',
+        negoQty: 0,
+        uom: 'TON',
+        bottomPrice: null,
+        unitPrice: 3250.0,
+        negotiationAmount: null,
+        var: '0%',
+        currency: 'USD',
+        awardStatus: '',
+      },
+    ],
+  },
+  {
+    materialCode: 'CH-1066',
+    materialDesc: 'Macro Fertilizer CaCO3',
+    quantity: 25,
+    uom: 'TON',
+    bottomPriceLBMA: 84051.25,
+    unitPriceLBMA: 3362.0,
+    simulationAmount: 79250.0,
+    variance: '-6%',
+    currency: 'USD',
+    awardDraft: '3 winner',
+    simulationStatus: 'Completed',
+    simulationMessage: 'Kumulasi Nego Qty melebihi Tender Qty, silakan disesuaikan kembali',
+    bids: [
+      {
+        rank: 1,
+        vendor: 'PT Walldorf Tbk',
+        negoQty: 10,
+        uom: 'TON',
+        bottomPrice: 33620.5,
+        unitPrice: 3100.0,
+        negotiationAmount: 31000.0,
+        var: '-8%',
+        currency: 'USD',
+        awardStatus: '🏅',
+      },
+      {
+        rank: 2,
+        vendor: 'PT Somper GmbH Tbk',
+        negoQty: 10,
+        uom: 'TON',
+        bottomPrice: 33620.5,
+        unitPrice: 3200.0,
+        negotiationAmount: 32000.0,
+        var: '-5%',
+        currency: 'USD',
+        awardStatus: '🏅',
+      },
+      {
+        rank: 3,
+        vendor: 'PT Kreuzschmid Kgaa Tbk',
+        negoQty: 5,
+        uom: 'TON',
+        bottomPrice: 16810.25,
+        unitPrice: 3250.0,
+        negotiationAmount: 16250.0,
+        var: '3%',
+        currency: 'USD',
+        awardStatus: '🏅',
+      },
+    ],
+  },
+]
+
 const currentTab = ref<string>('awardingDraft')
+
+const updateQty = (data: string) => {
+  console.log(data)
+}
 </script>
 
 <style lang="scss" scoped>
