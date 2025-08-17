1414cfb841d88a56be12a66ebec7c7cdc997cb0f
Firdaus Mochamad <medioid13@gmail.com>
2025-08-17 21:01:22 +0700

    ajustment evaluation adn data

--- src/views/tenderEvaluation/TenderEvaluation.vue
+++ src/views/tenderEvaluation/TenderEvaluation.vue
@@ -5,6 +5,9 @@
       <StepperStatusTender activeName="Vendor Awarding" />
     </div>
     <div class="mt-[24px] border rounded-lg p-[24px]">
+      <AwardingDraft :data="tenderEvoStore.dummyData" @updateCount="updateData" />
+    </div>
+    <div class="mt-[24px] border rounded-lg p-[24px]">
       <VendorTable :data="dummyData" />
     </div>
     <div class="mt-[24px] border rounded-lg p-[24px]">
@@ -25,12 +28,20 @@ import StepperStatusTender from '@/components/stepperStatusTender/StepperStatusT
 import Breadcrumb from '@/components/BreadcrumbView.vue'
 import type { routeTypes } from '@/core/type/components/breadcrumb'
 import type { VendorListTypes, DetailVendorParentTypes } from './types/tenderEvaluation'
+import { useTenderEvoStore } from '@/stores/tender-evo/tenderNegotiation'
+import AwardingDraft from '../tenderReportNegotiation/tenderReportNegotiation/AwardingDraft.vue'
+
+const tenderEvoStore = useTenderEvoStore()
+
+const updateData = (row: any, bid: any) => {
+  tenderEvoStore.updateQty(row, bid)
+}
 
 const routes = ref<routeTypes[]>([
   {
     name: 'Evaluation',
-    to: '/tender/evaluation'
-  }
+    to: '/tender/evaluation',
+  },
 ])
 
 const dummyData = ref<VendorListTypes[]>([
@@ -46,7 +57,7 @@ const dummyData = ref<VendorListTypes[]>([
     vendorDisc: '30%',
     discAmout: 51300,
     totalNettAmount: 119700,
-    remarks: 'Tahun kedua akan increase 10% tapi memasukkan parameter inflasi'
+    remarks: 'Tahun kedua akan increase 10% tapi memasukkan parameter inflasi',
   },
   {
     id: 2,
@@ -60,7 +71,7 @@ const dummyData = ref<VendorListTypes[]>([
     vendorDisc: '25%',
     discAmout: 42750,
     totalNettAmount: 128250,
-    remarks: 'Tahun kedua ada kemungkinan increase 5%'
+    remarks: 'Tahun kedua ada kemungkinan increase 5%',
   },
   {
     id: 3,
@@ -74,8 +85,8 @@ const dummyData = ref<VendorListTypes[]>([
     vendorDisc: '20%',
     discAmout: 34200,
     totalNettAmount: 136800,
-    remarks: 'Dapat melakukan fixed rate 3 Years'
-  }
+    remarks: 'Dapat melakukan fixed rate 3 Years',
+  },
 ])
 
 const dummyDataDetail = ref<DetailVendorParentTypes[]>([
@@ -91,46 +102,48 @@ const dummyDataDetail = ref<DetailVendorParentTypes[]>([
       {
         evaluationItem: 'Kualitas Produk/Layanan',
         weight: '20%',
-        description: 'Kualitas produk atau layanan yang diberikan, bebas dari cacat atau kesalahan.',
-        expectedSla: 'Tingkat cacat (defect rate) < 1%'
+        description:
+          'Kualitas produk atau layanan yang diberikan, bebas dari cacat atau kesalahan.',
+        expectedSla: 'Tingkat cacat (defect rate) < 1%',
       },
       {
         evaluationItem: 'Ketepatan Waktu Pengiriman',
         weight: '15%',
         description: 'Kepatuhan vendor terhadap jadwal pengiriman yang telah disepakati.',
-        expectedSla: 'Tingkat pengiriman tepat waktu > 98%'
+        expectedSla: 'Tingkat pengiriman tepat waktu > 98%',
       },
       {
         evaluationItem: 'Kepatuhan SLA',
         weight: '15%',
         description: 'Kepatuhan terhadap semua persyaratan SLA lainnya dalam kontrak.',
-        expectedSla: 'Tingkat kepatuhan keseluruhan > 95%'
+        expectedSla: 'Tingkat kepatuhan keseluruhan > 95%',
       },
       {
         evaluationItem: 'Komunikasi & Responsivitas',
         weight: '10%',
-        description: 'Seberapa cepat dan efektif vendor merespons pertanyaan, masalah, atau permintaan.',
-        expectedSla: 'Waktu respons (response time) < 2 jam kerja'
+        description:
+          'Seberapa cepat dan efektif vendor merespons pertanyaan, masalah, atau permintaan.',
+        expectedSla: 'Waktu respons (response time) < 2 jam kerja',
       },
       {
         evaluationItem: 'Manajemen Masalah',
         weight: '10%',
         description: 'Efektivitas vendor dalam menyelesaikan masalah yang muncul.',
-        expectedSla: 'Waktu penyelesaian (resolution time) < 24 jam kerja'
+        expectedSla: 'Waktu penyelesaian (resolution time) < 24 jam kerja',
       },
       {
         evaluationItem: 'Inovasi & Peningkatan',
         weight: '10%',
         description: 'Kemampuan vendor untuk memberikan ide atau solusi baru untuk peningkatan.',
-        expectedSla: 'Minimal 1 ide inovasi per tahun'
+        expectedSla: 'Minimal 1 ide inovasi per tahun',
       },
       {
         evaluationItem: 'Kepatuhan Regulasi',
         weight: '10%',
         description: 'Kepatuhan vendor terhadap semua peraturan dan standar industri yang berlaku.',
-        expectedSla: 'Nol pelanggaran regulasi'
-      }
-    ]
+        expectedSla: 'Nol pelanggaran regulasi',
+      },
+    ],
   },
   {
     id: 2,
@@ -145,52 +158,52 @@ const dummyDataDetail = ref<DetailVendorParentTypes[]>([
         evaluationItem: 'Dukungan Teknis',
         weight: '10%',
         description: 'Ketersediaan dan kualitas dukungan teknis yang diberikan.',
-        expectedSla: 'Ketersediaan dukungan 24/7'
+        expectedSla: 'Ketersediaan dukungan 24/7',
       },
       {
         evaluationItem: 'Stabilitas Keuangan',
         weight: '20%',
         description: 'Kesehatan dan stabilitas keuangan vendor untuk memastikan keberlanjutan.',
-        expectedSla: 'Tidak ada masalah hukum/finansial besar'
+        expectedSla: 'Tidak ada masalah hukum/finansial besar',
       },
       {
         evaluationItem: 'Struktur Biaya',
         weight: '15%',
         description: 'Kejelasan dan transparansi struktur biaya yang ditawarkan.',
-        expectedSla: 'Rincian biaya transparan dan terperinci'
+        expectedSla: 'Rincian biaya transparan dan terperinci',
       },
       {
         evaluationItem: 'Termin Pembayaran',
         weight: '15%',
         description: 'Fleksibilitas dan syarat pembayaran yang ditawarkan.',
-        expectedSla: 'Termin pembayaran 30 hari'
+        expectedSla: 'Termin pembayaran 30 hari',
       },
       {
         evaluationItem: 'Prakiraan Biaya',
         weight: '10%',
         description: 'Akurasi vendor dalam memperkirakan biaya di awal.',
-        expectedSla: 'Akurasi prakiraan biaya > 95%'
+        expectedSla: 'Akurasi prakiraan biaya > 95%',
       },
       {
         evaluationItem: 'Nilai Tambah',
         weight: '10%',
         description: 'Layanan atau fitur tambahan yang diberikan tanpa biaya ekstra.',
-        expectedSla: 'Diskon volume > 5%'
+        expectedSla: 'Diskon volume > 5%',
       },
       {
         evaluationItem: 'Fleksibilitas Harga',
         weight: '5%',
         description: 'Kesediaan vendor untuk negosiasi harga.',
-        expectedSla: 'Bersedia negosiasi ulang setiap tahun'
+        expectedSla: 'Bersedia negosiasi ulang setiap tahun',
       },
       {
         evaluationItem: 'Diskon Volume',
         weight: '15%',
         description: 'Ketersediaan diskon untuk pembelian dalam jumlah besar.',
-        expectedSla: 'Diskon volume untuk setiap kenaikan 10%'
-      }
-    ]
-  }
+        expectedSla: 'Diskon volume untuk setiap kenaikan 10%',
+      },
+    ],
+  },
 ])
 
 const columns = ref<string[]>([
@@ -199,15 +212,10 @@ const columns = ref<string[]>([
   'Evaluation Item',
   'Weight',
   'Description',
-  'Expected SLA'
+  'Expected SLA',
 ])
 
-const childColumns = ref<string[]>([
-  'Evaluation Item',
-  'Weight',
-  'Description',
-  'Expected SLA'
-])
+const childColumns = ref<string[]>(['Evaluation Item', 'Weight', 'Description', 'Expected SLA'])
 
 const addVendorToDetail = () => {
   for (const item of dummyData.value) {
@@ -253,8 +261,8 @@ watch(
   },
   {
     deep: true,
-    immediate: true
-  }
+    immediate: true,
+  },
 )
 
 watch(
@@ -266,8 +274,8 @@ watch(
   },
   {
     deep: true,
-    immediate: true
-  }
+    immediate: true,
+  },
 )
 
 onMounted(() => {
