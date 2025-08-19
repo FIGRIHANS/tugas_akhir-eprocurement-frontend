7ee33025c0b623e18a84075b16739a20315c2130
Firdaus Mochamad <medioid13@gmail.com>
2025-08-19 13:16:40 +0700

    ajustment v3

--- src/views/vendorAwarding/vendorAwarding.vue
+++ src/views/vendorAwarding/vendorAwarding.vue
@@ -20,7 +20,14 @@
       <UiButton> Approve Awarding </UiButton>
     </section>
     <section>
-      <ParticipatingVendor class="mt-[24px]" />
+      <VendorTable :data="dummyData" />
+    </section>
+    <section class="mt-[24px] border rounded-lg p-[24px]">
+      <DetailVendorTable
+        :data="dummyDataDetail"
+        :columns-parent="columns"
+        :columns-child="childColumns"
+      />
     </section>
     <!-- <section
       name="table"
@@ -116,11 +123,35 @@
       </div>
     </section> -->
   </div>
+
+  <div class="mt-5">
+    <p class="font-semibold">Signature</p>
+    <div class="flex gap-[16px] mt-[16px]">
+      <Vue3Signature
+        ref="signatureRef"
+        :sigOption="optionSignature"
+        :w="'346px'"
+        :h="'220px'"
+        class="border rounded-2xl border-gray-300 overflow-hidden"
+      />
+      <div class="flex flex-col justify-between">
+        <div>
+          <p class="text-sm text-gray-600">PIC Tender</p>
+          <p class="mt-[10px] text-sm">Ahmad Sambudi</p>
+        </div>
+        <button class="btn btn-outline btn-primary" @click="clearSignature">
+          <i class="ki-duotone ki-arrows-circle"></i>
+          Clear
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 //   import LPagination from '@/components/pagination/LPagination.vue'
 import Breadcrumb from '@/components/BreadcrumbView.vue'
+import Vue3Signature from 'vue3-signature'
 // import InputSearch from '@/components/ui/atoms/inputSearch/UiInputSearch.vue'
 // import UiChips from '@/components/ui/atoms/chips/UiChips.vue'
 import type { routeTypes } from '@/core/type/components/breadcrumb'
@@ -129,7 +160,9 @@ import StepperStatusTender from '@/components/stepperStatusTender/StepperStatusT
 import UiButton from '@/components/ui/atoms/button/UiButton.vue'
 import AwardingDraft from '@/views/tenderReportNegotiation/tenderReportNegotiation/AwardingDraft.vue'
 import { useTenderEvoStore } from '@/stores/tender-evo/tenderNegotiation'
-import ParticipatingVendor from '../tenderReportNegotiation/tenderReportNegotiation/ParticipatingVendor.vue'
+import type { DetailVendorParentTypes } from '../tenderEvaluation/types/tenderEvaluation'
+import DetailVendorTable from '../tenderEvaluation/TenderEvaluation/DetailVendorTable.vue'
+import VendorTable from './vendorTable/VendorTable.vue'
 
 const tenderEvoStore = useTenderEvoStore()
 
@@ -137,7 +170,24 @@ const search = ref('')
 const selectedItems = ref<number | null>(null)
 const currentStatus = ref<number>(1)
 const tenderStatus = ref<string>('open')
+const signatureRef = ref()
 //   const page = ref(1)
+
+const optionSignature = ref({
+  penColor: 'rgb(0, 0, 0)',
+  backgroundColor: 'rgba(249, 249, 249)',
+})
+
+const columns = ref<string[]>([
+  '',
+  'Evaluation Type',
+  'Evaluation Item',
+  'Weight',
+  'Description',
+  'Expected SLA',
+])
+
+const childColumns = ref<string[]>(['Evaluation Item', 'Weight', 'Description', 'Expected SLA'])
 
 const data = ref([
   {
@@ -225,22 +275,124 @@ const handleCheckboxChange = (id: number) => {
   }
 }
 
-// const isIndeterminate = computed(() => {
-//   return selectedItems.value.length > 0 && selectedItems.value.length < data.value.length
-// })
+const clearSignature = () => {
+  signatureRef.value.clear()
+}
 
-// watch(filter, () => {
-//     hasActiveFilter()
-// }, { immediate: true })
-
-// watch(
-//   selectedItems,
-//   () => {
-//     const selectAllCheckbox = document.querySelector('thead input[type="checkbox"]')
-//     if (selectAllCheckbox) {
-//       ;(selectAllCheckbox as HTMLInputElement).indeterminate = isIndeterminate.value
-//     }
-//   },
-//   { immediate: true },
-// )
+const dummyDataDetail = ref<DetailVendorParentTypes[]>([
+  {
+    id: 1,
+    evaluationType: 'Technical Evaluation',
+    evaluationItem: '',
+    weight: '60%',
+    description: '',
+    expectedSla: '',
+    isOpenChild: false,
+    children: [
+      {
+        evaluationItem: 'Kualitas Produk/Layanan',
+        weight: '20%',
+        description:
+          'Kualitas produk atau layanan yang diberikan, bebas dari cacat atau kesalahan.',
+        expectedSla: 'Tingkat cacat (defect rate) < 1%',
+      },
+      {
+        evaluationItem: 'Ketepatan Waktu Pengiriman',
+        weight: '15%',
+        description: 'Kepatuhan vendor terhadap jadwal pengiriman yang telah disepakati.',
+        expectedSla: 'Tingkat pengiriman tepat waktu > 98%',
+      },
+      {
+        evaluationItem: 'Kepatuhan SLA',
+        weight: '15%',
+        description: 'Kepatuhan terhadap semua persyaratan SLA lainnya dalam kontrak.',
+        expectedSla: 'Tingkat kepatuhan keseluruhan > 95%',
+      },
+      {
+        evaluationItem: 'Komunikasi & Responsivitas',
+        weight: '10%',
+        description:
+          'Seberapa cepat dan efektif vendor merespons pertanyaan, masalah, atau permintaan.',
+        expectedSla: 'Waktu respons (response time) < 2 jam kerja',
+      },
+      {
+        evaluationItem: 'Manajemen Masalah',
+        weight: '10%',
+        description: 'Efektivitas vendor dalam menyelesaikan masalah yang muncul.',
+        expectedSla: 'Waktu penyelesaian (resolution time) < 24 jam kerja',
+      },
+      {
+        evaluationItem: 'Inovasi & Peningkatan',
+        weight: '10%',
+        description: 'Kemampuan vendor untuk memberikan ide atau solusi baru untuk peningkatan.',
+        expectedSla: 'Minimal 1 ide inovasi per tahun',
+      },
+      {
+        evaluationItem: 'Kepatuhan Regulasi',
+        weight: '20%',
+        description: 'Kepatuhan vendor terhadap semua peraturan dan standar industri yang berlaku.',
+        expectedSla: 'Nol pelanggaran regulasi',
+      },
+    ],
+  },
+  {
+    id: 2,
+    evaluationType: 'Commercial Evaluation',
+    evaluationItem: '',
+    weight: '40%',
+    description: '',
+    expectedSla: '',
+    isOpenChild: false,
+    children: [
+      {
+        evaluationItem: 'Dukungan Teknis',
+        weight: '10%',
+        description: 'Ketersediaan dan kualitas dukungan teknis yang diberikan.',
+        expectedSla: 'Ketersediaan dukungan 24/7',
+      },
+      {
+        evaluationItem: 'Stabilitas Keuangan',
+        weight: '20%',
+        description: 'Kesehatan dan stabilitas keuangan vendor untuk memastikan keberlanjutan.',
+        expectedSla: 'Tidak ada masalah hukum/finansial besar',
+      },
+      {
+        evaluationItem: 'Struktur Biaya',
+        weight: '15%',
+        description: 'Kejelasan dan transparansi struktur biaya yang ditawarkan.',
+        expectedSla: 'Rincian biaya transparan dan terperinci',
+      },
+      {
+        evaluationItem: 'Termin Pembayaran',
+        weight: '15%',
+        description: 'Fleksibilitas dan syarat pembayaran yang ditawarkan.',
+        expectedSla: 'Termin pembayaran 30 hari',
+      },
+      {
+        evaluationItem: 'Prakiraan Biaya',
+        weight: '10%',
+        description: 'Akurasi vendor dalam memperkirakan biaya di awal.',
+        expectedSla: 'Akurasi prakiraan biaya > 95%',
+      },
+      {
+        evaluationItem: 'Nilai Tambah',
+        weight: '10%',
+        description: 'Layanan atau fitur tambahan yang diberikan tanpa biaya ekstra.',
+        expectedSla: 'Diskon volume > 5%',
+      },
+      {
+        evaluationItem: 'Fleksibilitas Harga',
+        weight: '5%',
+        description: 'Kesediaan vendor untuk negosiasi harga.',
+        expectedSla: 'Bersedia negosiasi ulang setiap tahun',
+      },
+      {
+        evaluationItem: 'Diskon Volume',
+        weight: '15%',
+        description: 'Ketersediaan diskon untuk pembelian dalam jumlah besar.',
+        expectedSla: 'Diskon volume untuk setiap kenaikan 10%',
+      },
+    ],
+  },
+])
 </script>
