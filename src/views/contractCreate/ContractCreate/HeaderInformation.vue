6ff26d56ef592b904033285c6acb6b00f77cfefe
Calvin Halim (PT Kamoro) <cavim1998@gmail.com>
2025-08-21 20:43:42 +0700

    clear unsued

--- src/views/contractCreate/ContractCreate/HeaderInformation.vue
+++ src/views/contractCreate/ContractCreate/HeaderInformation.vue
@@ -3,15 +3,11 @@
     <div class="border rounded-md p-[24px]">
       <GeneralData />
     </div>
-    <div class="border rounded-md p-[24px] mt-[24px]">
-      <TableItem />
-    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import GeneralData from './HeaderInformation/GeneralData.vue'
-import TableItem from './HeaderInformation/TableItem.vue'
 </script>
 
 <style lang="scss" scoped>
