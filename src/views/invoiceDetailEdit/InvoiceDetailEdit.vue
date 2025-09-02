d272427b5653fd73603f1a355bc323e8a6e24d03
Calvin Halim (PT Kamoro) <cavim1998@gmail.com>
2025-09-02 09:47:25 +0700

    add id po gr detail edit

--- src/views/invoiceDetailEdit/InvoiceDetailEdit.vue
+++ src/views/invoiceDetailEdit/InvoiceDetailEdit.vue
@@ -571,6 +571,7 @@ const mapPoGr = () => {
   const poGr = []
   for (const item of form.value.invoicePoGr) {
     poGr.push({
+      id: item.id,
       poNo: item.poNo,
       poItem: Number(item.poItem),
       grDocumentNo: item.grDocumentNo,
