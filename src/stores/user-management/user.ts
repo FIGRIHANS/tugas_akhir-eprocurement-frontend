17ddc996a18a870d5f1733421912108106ebfc62
Kuswari Datasea <kuswari@datasea.co.id>
2025-10-21 23:55:20 +0800

    integrate user detail

--- src/stores/user-management/user.ts
+++ src/stores/user-management/user.ts
@@ -1,33 +1,36 @@
-import { defineStore } from "pinia";
-import type { IUser } from "./types/user";
-import { ref } from "vue";
-import type { ApiResponse } from "@/core/type/api";
-import userApi from "@/core/utils/userApi";
+import { defineStore } from 'pinia'
+import type { IUser } from './types/user'
+import { ref } from 'vue'
+import type { ApiResponse } from '@/core/type/api'
+import userApi from '@/core/utils/userApi'
 
 interface UserData {
-  items: IUser[];
-  total: number;
-  page: number;
-  pageSize: number;
+  items: IUser[]
+  total: number
+  page: number
+  pageSize: number
 }
 
 export const useUserStore = defineStore('userStore', () => {
-  const loading = ref(false);
-  const error = ref<string | null>(null);
+  const loading = ref(false)
+  const error = ref<string | null>(null)
   const users = ref<UserData>({
     items: [],
     total: 0,
     page: 0,
     pageSize: 0,
-  });
+  })
+
+  const userDetail = ref(null)
+
+  const userInformation = ref(null)
 
   const getAllUsers = async () => {
-    loading.value = true;
-    error.value = null;
+    loading.value = true
+    error.value = null
 
     try {
-
-      const response: ApiResponse<IUser[]> = await userApi.post('/user/getall');
+      const response: ApiResponse<IUser[]> = await userApi.get('/user/getall')
 
       if (response.data.result.isError) {
         error.value = response.data.result.message || 'An unknown error occurred.'
@@ -50,8 +53,6 @@ export const useUserStore = defineStore('userStore', () => {
         }
         error.value = 'API response content is malformed or missing expected array of users.'
       }
-
-
     } catch (err: unknown) {
       if (err instanceof Error) {
         error.value = err.message
@@ -63,12 +64,44 @@ export const useUserStore = defineStore('userStore', () => {
     }
   }
 
+  const getUserInformation = async (employeeId: number) => {
+    loading.value = true
+    error.value = null
+    try {
+      const response: ApiResponse<unknown> = await userApi.get(`/user/information/${employeeId}`)
+
+      console.log(response)
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        error.value = err.message
+      }
+    } finally {
+      loading.value = false
+    }
+  }
+
+  const getUserDetail = async (userName: string) => {
+    loading.value = true
+    error.value = null
+    try {
+      const response: ApiResponse<unknown> = await userApi.get(`/user?userName=${userName}`)
+
+      userDetail.value = response?.data?.result?.content
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        error.value = err.message
+      }
+    } finally {
+      loading.value = false
+    }
+  }
+
   const storeUserData = async (body: any) => {
     loading.value = true
     error.value = null
     try {
-      const response: ApiResponse<any> = await userApi.post('/auth', {
-        ...body
+      const response: ApiResponse<unknown> = await userApi.post('/auth', {
+        ...body,
       })
 
       if (response.data.result.isError) {
@@ -77,7 +110,6 @@ export const useUserStore = defineStore('userStore', () => {
       }
 
       return response.data.result
-
     } catch (err: unknown) {
       if (err instanceof Error) {
         error.value = err.message
@@ -94,8 +126,11 @@ export const useUserStore = defineStore('userStore', () => {
     loading,
     error,
 
-    getAllUsers,
-    storeUserData
-  }
+    userDetail,
 
-});
+    getAllUsers,
+    getUserInformation,
+    getUserDetail,
+    storeUserData,
+  }
+})
