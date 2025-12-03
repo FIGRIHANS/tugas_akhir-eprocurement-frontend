7d3072bd5e48de06939270088ca3aee837065085
Calvin Halim <cavim1998@gmail.com>
2025-12-03 08:54:03 +0700

    adjust deploy

--- src/core/utils/invoiceApi.ts
+++ src/core/utils/invoiceApi.ts
@@ -1,29 +1,29 @@
 import axios from 'axios'
-import { getToken } from '@/composables/token'
+// import { getToken } from '@/composables/token'
 
 const invoiceApi = axios.create({
   baseURL: import.meta.env.VITE_API_INVOICE_BASE_URL,
 })
 
-invoiceApi.interceptors.request.use(
-  (config) => {
-    let token = getToken()
-    if (!token) {
-      try {
-        const sessionData = localStorage.getItem('session_data')
-        if (sessionData) {
-          const params = new URLSearchParams(sessionData)
-          const t = params.get('token_dts')
-          if (t) token = t
-        }
-      } catch {
-        // ignore
-      }
-    }
+// invoiceApi.interceptors.request.use(
+//   (config) => {
+//     let token = getToken()
+//     if (!token) {
+//       try {
+//         const sessionData = localStorage.getItem('session_data')
+//         if (sessionData) {
+//           const params = new URLSearchParams(sessionData)
+//           const t = params.get('token_dts')
+//           if (t) token = t
+//         }
+//       } catch {
+//         // ignore
+//       }
+//     }
 
-    if (token) config.headers.Authorization = token
-    return config
-  }
-)
+//     if (token) config.headers.Authorization = token
+//     return config
+//   }
+// )
 
 export default invoiceApi
