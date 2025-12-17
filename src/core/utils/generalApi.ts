a83ede727732ab4b6fc03e722b1d17c06681d2b6
Firdaus Mochamad <medioid13@gmail.com>
2025-12-17 08:09:00 +0700

    ajust token

--- src/core/utils/generalApi.ts
+++ src/core/utils/generalApi.ts
@@ -1,7 +1,27 @@
 import axios from 'axios'
+import { getToken } from '@/composables/token'
 
 const generalApi = axios.create({
   baseURL: import.meta.env.VITE_API_GENERAL_BASE_URL,
+})
+
+generalApi.interceptors.request.use((config) => {
+  let token = getToken()
+  if (!token) {
+    try {
+      const sessionData = localStorage.getItem('session_data')
+      if (sessionData) {
+        const params = new URLSearchParams(sessionData)
+        const t = params.get('token_dts')
+        if (t) token = t
+      }
+    } catch {
+      // ignore
+    }
+  }
+
+  if (token) config.headers.Authorization = token
+  return config
 })
 
 export default generalApi
